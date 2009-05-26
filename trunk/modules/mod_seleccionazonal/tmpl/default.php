99bad518ed56e11964c060afa8fc7b28ba9087d5
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-05-26 13:05:33 +0000

    Se agregan variables task y option en el formulario del componente seleccionazonal

--- trunk/modules/mod_seleccionazonal/tmpl/default.php
+++ trunk/modules/mod_seleccionazonal/tmpl/default.php
@@ -9,13 +9,16 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 <img src="templates/<?php echo $template; ?>/images/title_inicio_seleccion.gif" border="0" alt="Seleccioná tu Zonal" />
 <div class="padd5">
 <p><?php echo JText::_('select_description'); ?></p>
-<form>
-  <select class="cmb" id="select" name="select">
+<form action="index.php" method="post" name="setZonalForm" id="setZonalForm">
+  <select class="cmb" id="selectZonal" name="selectZonal">
     <?php foreach ($zonales as $zonal): ?>
     <option value="<?php echo $zonal->id; ?>"><?php echo $zonal->label; ?></option>  
     <?php endforeach; ?>
   </select>
-  <input type="image" src="templates/<?php echo $template; ?>/images/bot_search_orange.gif" class="submit" value="Submit" id="button" name="button"/>
+  <input type="image" src="templates/<?php echo $template; ?>/images/bot_search_orange.gif" class="submit" name="button" title="title_here" />
+  <input type="hidden" name="task" value="setZonal" />
+  <input type="hidden" name="option" value="com_userzonales" />
+  <?php echo JHTML::_('form.token'); ?>
 </form>
 </div><!-- end .padd5 -->
 </div><!-- end #selZ-l -->
