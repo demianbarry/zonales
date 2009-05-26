77721e7e8ab045ac1a1662115ee0a86b6481399c
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-05-26 13:07:21 +0000

    Componente userzonales. Implementará lógica necesaria para la ecualización transitoria por zonas. Se agregan también los modelos a modificar en el componente core de Joomla! com_content.

---
+++ components/com_userzonales/controller.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version		$Id: controller.php 11215 2008-10-26 02:25:51Z ian $
+ * @package		Zonales
+ * @copyright		Mediabit	
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant to the
+ * GNU General Public License, and as distributed it includes or is derivative
+ * of works licensed under the GNU General Public License or other free or open
+ * source software licenses. See COPYRIGHT.php for copyright notices and
+ * details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+jimport('joomla.application.component.controller');
+
+/**
+ * User Zonales
+ *
+ * @package		Zonales
+ * @since 1.5
+ */
+class UserZonalesController extends JController
+{
+	function zonal()
+	{
+		global $option;
+		$document = &JFactory::getDocument();
+		$vType = $document->getType();
+		$vName = JRequest::getCmd('view','zonal');
+		$vLayout = JRequest::getCmd('layout','default');
+
+		$view =& $this->getView($vName, $vType);
+		$view->setLayout($vLayout);
+		$view->display();
+	}
+
+	function setZonal()
+	{
+		global $option;
+
+		$link = JRoute::_('index.php');
+		$this->setRedirect($link);
+	}
+}
