5c0087ea069f37480441484162208449d885c06a
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-05-27 15:57:14 +0000

    Simplificación del template de mod_seleccionazonal

--- trunk/modules/mod_seleccionazonal/tmpl/default.php
+++ trunk/modules/mod_seleccionazonal/tmpl/default.php
@@ -4,10 +4,6 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
 <div id="seleccionaZ">
-<div id="selZ-container">
-<div id="selZ-l">
-<img src="templates/<?php echo $template; ?>/images/title_inicio_seleccion.gif" border="0" alt="Seleccioná tu Zonal" />
-<div class="padd5">
 <p><?php echo JText::_('SELECCIONA_ZONAL'); ?></p>
 <form action="index.php" method="post" name="setZonalForm" id="setZonalForm">
   <select class="cmb" id="selectZonal" name="selectZonal">
@@ -20,8 +16,5 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
   <input type="hidden" name="option" value="com_userzonales" />
   <?php echo JHTML::_('form.token'); ?>
 </form>
-</div><!-- end .padd5 -->
-</div><!-- end #selZ-l -->
-</div><!-- end #selZ-container -->
 </div><!-- end #seleccionaZ -->
 
