510ca411400868df8f2d0408eaef2659cf8db533
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-05-27 17:45:38 +0000

    Se agregan opciones que modifican el comportamiento del modulo

--- trunk/modules/mod_seleccionazonal/mod_seleccionazonal.php
+++ trunk/modules/mod_seleccionazonal/mod_seleccionazonal.php
@@ -4,11 +4,19 @@ defined('_JEXEC') or die('Restricted access');
 
 require_once (dirname(__FILE__).DS.'helper.php');
 
+// lista de zonales, zonal actualmente seleccionado
 $helper = new modSeleccionaZonalHelper();
 $zonales =& $helper->getZonales();
 $zonal_id = $helper->getZonalActual();
 
+// template
 $app =& JFactory::getApplication();
 $template = $app->getTemplate();
+
+// parametros
+$showHeader = $params->get('show_header');
+$useCustomHeader = $params->get('custom_header');
+$customHeaderText = $params->get('header_text');
+$useSubmitButton = $params->get('use_submit_button');
 
 require(JModuleHelper::getLayoutPath('mod_seleccionazonal'));
--- trunk/modules/mod_seleccionazonal/tmpl/default.php
+++ trunk/modules/mod_seleccionazonal/tmpl/default.php
@@ -4,14 +4,24 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
 <div id="seleccionaZ">
-<p><?php echo JText::_('SELECCIONA_ZONAL'); ?></p>
+<p>
+<?php if($showHeader): ?>
+  <?php if($useCustomHeader): ?>
+    <?php echo $customHeaderText; ?>
+  <?php else: ?>
+    <?php echo JText::_('SELECCIONA_ZONAL'); ?>
+  <?php endif; ?>
+<?php endif; ?>
+</p>
 <form action="index.php" method="post" name="setZonalForm" id="setZonalForm">
-  <select class="cmb" id="selectZonal" name="selectZonal">
+  <select class="cmb" id="selectZonal" name="selectZonal" <?php if (!$useSubmitButton) {echo 'OnChange="document.setZonalForm.submit()"';} ?> >
     <?php foreach ($zonales as $zonal): ?>
     <option value="<?php echo $zonal->id; ?>" <?php echo ($zonal_id == $zonal->id ? 'selected="selected"' : '') ?>><?php echo $zonal->label; ?></option>  
     <?php endforeach; ?>
   </select>
+  <?php if (!$useSubmitButton) echo "<noscript>"; ?>
   <input type="image" src="templates/<?php echo $template; ?>/images/bot_search_orange.gif" class="submit" name="button" title="title_here" />
+  <?php if (!$useSubmitButton) echo "</noscript>"; ?>
   <input type="hidden" name="task" value="setZonal" />
   <input type="hidden" name="option" value="com_userzonales" />
   <?php echo JHTML::_('form.token'); ?>
