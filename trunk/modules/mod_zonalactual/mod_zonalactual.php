b4c6d974f9819ce022286e2c1d6f5c47817c5297
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-05-29 19:04:08 +0000

    Se agregar modulo mod_zonalactual que informa del zonal actual

---
+++ trunk/modules/mod_zonalactual/mod_zonalactual.php
@@ -0,0 +1,22 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+//require_once (dirname(__FILE__).DS.'helper.php');
+require_once (JPATH_BASE.DS.'com_zonales'.DS.'helper.php');
+
+// lista de zonales, zonal actualmente seleccionado
+//$helper = new modZonalActualHelper();
+$helper = new comZonalesHelper();
+$zonal_id = $helper->getZonalActual();
+$zonal_name = $helper->getZonal($zonal_id);
+
+// parametros
+$showLabel = $params->get('show_label');
+$labelText = $params->get('label_text');
+
+// template
+$app =& JFactory::getApplication();
+$template = $app->getTemplate();
+
+require(JModuleHelper::getLayoutPath('mod_zonalactual'));
---
+++ trunk/modules/mod_zonalactual/tmpl/default.php
@@ -0,0 +1,8 @@
+<?php
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die ( 'Restricted Access' );
+?>
+
+<div id="moduletable_zName">
+    <p><?php if($showLabel) { echo $textLabel; } echo $zonal_name; ?></p>
+</div>
