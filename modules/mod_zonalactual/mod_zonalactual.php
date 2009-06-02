dbb39ab4baab375511de0035c0ea8762cabc7a77
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-02 13:20:50 +0000

    El módulo mod_zonalactual muestra un texto acorde en caso de que no se haya seleccionado ningún zonal aún

--- modules/mod_zonalactual/mod_zonalactual.php
+++ modules/mod_zonalactual/mod_zonalactual.php
@@ -5,16 +5,14 @@ defined('_JEXEC') or die('Restricted access');
 //require_once (dirname(__FILE__).DS.'helper.php');
 require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 
+// parametros
+$showLabel = $params->get('show_label', 'show_label');
+$labelText = $params->get('label_text', 'label_text');
+
 // lista de zonales, zonal actualmente seleccionado
 $helper = new comZonalesHelper();
 $zonal_id = $helper->getZonalActual();
-if ($zonal_id) {
-	$zonal_name = $helper->getZonal($zonal_id);
-}
-
-// parametros
-$showLabel = $params->get('show_label');
-$labelText = $params->get('label_text');
+$zonal_name = $zonal_id ? $helper->getZonal($zonal_id) : $params->get('nozonal_text');
 
 // template
 $app =& JFactory::getApplication();
