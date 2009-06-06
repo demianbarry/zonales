2f5d6367a90df33f7afc80ea56f29ffe1b3d57a3
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-06 05:25:35 +0000

    Se documentan los métodos. getZonal(int) recupera información del zonal actual en caso de no pasarse ningún argumento o si este fuese NULL.

--- trunk/components/com_zonales/helper.php
+++ trunk/components/com_zonales/helper.php
@@ -4,6 +4,11 @@ defined('_JEXEC') or die('Restricted access');
 
 class comZonalesHelper
 {
+	/**
+	 * Recupera una lista de los zonales actualmente disponibles
+	 *
+	 * @return array Lista de zonales recuperados
+	 */
 	function getZonales() 
 	{
 	  	$dbo	= & JFactory::getDBO();
@@ -17,14 +22,34 @@ class comZonalesHelper
 		return $zonales;
 	}
 
+	/**
+	 * Retorna el identificador del zonal actual de la sesión, o NULL en 
+	 * caso de que no se encuentre seteado ninguno.
+	 * 
+	 * @return  int Identificador del zonal actual
+	 */
 	function getZonalActual()
 	{
 		$session = JFactory::getSession();
 		return $session->get('zonales_zonal_id', NULL);
 	}
 
-	function getZonal($zonal_id)
+	/**
+	 * Recupera información acerca de un zonal en particular. Si no se
+	 * especifica un identificador se recuperará información acerca del
+	 * zonal actual.
+	 * 
+	 * @param int Identificador del zonal
+	 * @return object Objeto con información acerca del zonal indicado
+	 */
+	function getZonal($zonal_id = NULL)
 	{
+		if (is_null($zonal_id))
+		{
+			$zonal_id = $this->getZonalActual();
+			if (is_null($zonal_id)) return NULL;
+		}
+
 	  	$dbo	= & JFactory::getDBO();
 		$query = 'SELECT ' . $dbo->nameQuote('f.id') .', '. $dbo->nameQuote('f.label')
                     	.' FROM ' . $dbo->nameQuote('#__custom_properties_fields') . ' f'
