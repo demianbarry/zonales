c3c865167dcecbc79e1f7c90f4c341d4ce171f79
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-08 19:48:42 +0000

    Mostrar mapa de zonales al usuario como popup.
    Se genera un módulo que permite, mediante un lightbox, mostrar al usuario el mapa de selección de zonales.

---
+++ modules/mod_mapazonales/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die ( 'Restricted Access' );
+?>
+
+<div id="moduletable_zName">
+    <p>
+	<?php if($showLabel) { echo $labelText . ' '; } ?>
+	<a rel="lightbox;width:800;height:600" title="Mediabit" href="index2.php?option=com_zonales&view=mapa">Mediabit</a>
+    </p>
+</div>
