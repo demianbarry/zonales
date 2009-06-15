1eed08a0bf3f667312f27ebccfdc8363f8a7ec40
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-15 15:29:19 +0000

    Visualización únicamente de la vista mapa del componente com_zonales.
    Se modifica la url generada por mod_mapazonales para que se muestre unicamente el html generado por el componente invocado.

--- trunk/modules/mod_mapazonales/tmpl/default.php
+++ trunk/modules/mod_mapazonales/tmpl/default.php
@@ -6,6 +6,6 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 <div id="moduletable_zName">
     <p>
 	<?php if($showLabel) { echo $labelText . ' '; } ?>
-	<a rel="lightbox;width:800;height:600" title="Zonales" href="index2.php?option=com_zonales&view=mapa&ajax=true">Mediabit</a>
+	<a rel="lightbox;width:800;height:600" title="Zonales" href="index.php?option=com_zonales&view=mapa&tmpl=component&ajax=true">Mediabit</a>
     </p>
 </div>
