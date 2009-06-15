e2ea127ddc237ce6381229327ea9d900e692288e
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-15 19:33:03 +0000

    Se agrega parametro para especificar título del popup en mod_mapazonales. Se utiliza "text" en lugar de "textarea" en donde corresponda.

--- modules/mod_mapazonales/mod_mapazonales.php
+++ modules/mod_mapazonales/mod_mapazonales.php
@@ -7,6 +7,7 @@ require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 // parametros
 $showLabel = $params->get('show_label', 'show_label');
 $labelText = $params->get('label_text', 'label_text');
+$titleText = $params->get('title_text', 'title_text');
 
 // template
 $app =& JFactory::getApplication();
