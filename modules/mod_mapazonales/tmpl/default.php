2a2fe0efdc1ad0a8a24d6095a2b3e99e1669c846
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-17 18:04:56 +0000

    Se agrega soporte en el template para mostrar solamente el contenido del componente actual.
    Esta modificación permite mostrar únicamente el contenido del componente actual sin el resto del template (menúes, módulos, etc). Para ello se debe agregar a la URL el parametro tmpl=component_only. 
    
    Se modifica también el componente mod_mapazonales para que genere un url de acuerdo a lo anterior.

--- modules/mod_mapazonales/tmpl/default.php
+++ modules/mod_mapazonales/tmpl/default.php
@@ -6,6 +6,6 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 <div id="moduletable_zName">
     <p>
 	<?php if($showLabel) { echo $labelText . ' '; } ?>
-	<a rel="lightbox;width:800;height:600" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component">Mediabit</a>
+	<a rel="lightbox;width:800;height:600" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
     </p>
 </div>
