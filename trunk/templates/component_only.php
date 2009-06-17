6456977af94a92c2da181c7b5dcd5b2c39955bc6
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-17 18:04:56 +0000

    Se agrega soporte en el template para mostrar solamente el contenido del componente actual.
    Esta modificación permite mostrar únicamente el contenido del componente actual sin el resto del template (menúes, módulos, etc). Para ello se debe agregar a la URL el parametro tmpl=component_only. 
    
    Se modifica también el componente mod_mapazonales para que genere un url de acuerdo a lo anterior.

---
+++ trunk/templates/component_only.php
@@ -0,0 +1,33 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+
+<?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+<jdoc:include type="head" />
+
+<link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/reset.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/main.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/content.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/modules.css">
+
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/swfobject.js"></script>
+
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/glassnav.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/glassnav.css">
+
+
+</head>
+
+<body>
+
+  <div id="containerContent">
+    <div id="container-l">
+      <div id="mainContent">
+        <jdoc:include type="component" />
+      </div> <!-- mainContent -->
+    </div> <!-- container-l -->
+  </div> <!-- containerContent -->
+
+</body>
+</html>
