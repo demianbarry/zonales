b7525c20644b6220e4ad05cae8552b40a7a98fbc
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-17 18:24:08 +0000

    Se agregan parametos de configuración al modulo mod_mapazonales.
    Los parametros nuevos especifican el tamaño (with & height) del popup lightbox. Se permite además especificar un custom.

--- modules/mod_mapazonales/mod_mapazonales.php
+++ modules/mod_mapazonales/mod_mapazonales.php
@@ -8,6 +8,8 @@ require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 $showLabel = $params->get('show_label', 'show_label');
 $labelText = $params->get('label_text', 'label_text');
 $titleText = $params->get('title_text', 'title_text');
+$height = $params->get('height_popup');
+$width = $params->get('width_popup');
 
 // template
 $app =& JFactory::getApplication();
--- modules/mod_mapazonales/tmpl/default.php
+++ modules/mod_mapazonales/tmpl/default.php
@@ -4,8 +4,9 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
 <div id="moduletable_zName">
-    <p>
-	<?php if($showLabel) { echo $labelText . ' '; } ?>
-	<a rel="lightbox;width:800;height:600" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
-    </p>
+	<p><?php echo $titleText; ?></p>
+	<p>
+		<?php if($showLabel) { echo $labelText . ' '; } ?>
+		<a rel="lightbox;width:<?php echo $height; ?>;height:<?php echo $width; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
+	</p>
 </div>
