8a8ead24ee76b686f85ff73bedef88c48a126720
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-17 20:12:20 +0000

    Administrador especifica tamaño del popup lightbox del modulo com_mapazonales.
    Un usuario administrador podrá especificar desde la configuración del módulo el tamaño (height & width) del popup.

--- modules/mod_mapazonales/tmpl/default.php
+++ modules/mod_mapazonales/tmpl/default.php
@@ -21,6 +21,6 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 	<p><?php echo $titleText; ?></p>
 	<p>
 		<?php if($showLabel) { echo $labelText . ' '; } ?>
-		<a id="zonal" rel="lightbox;width:<?php echo $height; ?>;height:<?php echo $width; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
+		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
 	</p>
 </div>
