f4cc02ce57c05fc6429b18c59a6e7e7c00c314ad
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-17 19:59:36 +0000

    Se refresca la página actual al cerrarse el lightbox con selección de zonal.
    Mediante javascript se especifica que al cerrar el lightbox (evento onClose) se ejecute un refresh en la ventana actual.

--- trunk/modules/mod_mapazonales/tmpl/default.php
+++ trunk/modules/mod_mapazonales/tmpl/default.php
@@ -3,10 +3,24 @@
 defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
+<script type="text/javascript">
+	// <![CDATA[
+
+	window.addEvent('domready', function() {
+		Shadowbox.init();
+
+		Shadowbox.setup($('zonal'), {
+			onClose:function() { window.location.reload(true); }
+		});
+	});
+
+	// ]]>
+</script>
+
 <div id="moduletable_zName">
 	<p><?php echo $titleText; ?></p>
 	<p>
 		<?php if($showLabel) { echo $labelText . ' '; } ?>
-		<a rel="lightbox;width:<?php echo $height; ?>;height:<?php echo $width; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
+		<a id="zonal" rel="lightbox;width:<?php echo $height; ?>;height:<?php echo $width; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
 	</p>
 </div>
