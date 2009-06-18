eda9d16c3bd991eab803564e0a7dad343615148d
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-18 18:06:28 +0000

    Opción para mostrar vista de componente o únicamente objeto flash en el popup de selección de zonal

--- modules/mod_mapazonales/tmpl/default.php
+++ modules/mod_mapazonales/tmpl/default.php
@@ -7,7 +7,21 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 	// <![CDATA[
 
 	window.addEvent('domready', function() {
-		Shadowbox.init();
+		Shadowbox.init({
+			skipSetup: true,
+			language: "es",
+			players: ['swf'],
+			flashParams: {
+				scale: "noscale",
+				allowScriptAccess: "sameDomain",
+				width: 400,
+				height: 500
+			},
+			flashVars: {
+				MY_ZONES: "bue_avellaneda",
+				URL_SET_ZONES: "index.php"
+			}
+		});
 
 		Shadowbox.setup($('zonal'), {
 			onClose:function() { window.location.reload(true); }
@@ -21,6 +35,10 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 	<p><?php echo $titleText; ?></p>
 	<p>
 		<?php if($showLabel) { echo $labelText . ' '; } ?>
+		<?php if(!$useFlash): ?>
 		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
+		<?php else: ?>
+		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="http://www.zonales.com.ar/zonales-devel/templates/z1/swf/map_1.0.swf">Mediabit</a>
+		<?php endif; ?>
 	</p>
 </div>
