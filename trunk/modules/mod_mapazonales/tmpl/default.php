e1d315dd681c450977acbf9dc3bdfc8d07b43979
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-26 16:31:33 +0000

    Se utiliza imágen en el link para el popup del mapa de selección de zonales.

--- trunk/modules/mod_mapazonales/tmpl/default.php
+++ trunk/modules/mod_mapazonales/tmpl/default.php
@@ -5,8 +5,11 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 
 <script type="text/javascript" defer="defer">
 	// <![CDATA[
-
 	window.addEvent('domready', function() {
+		Shadowbox.init({
+			skipSetup: true
+		});
+
 		Shadowbox.setup($('zonal'), {
 			onClose:function() { window.location.reload(true); }
 		});
@@ -16,13 +19,14 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 </script>
 
 <div id="moduletable_zName">
-	<p><?php echo $titleText; ?></p>
+	<?php if($showLabel): ?>
 	<p>
-		<?php if($showLabel) { echo $labelText . ' '; } ?>
-		<?php if(!$useFlash): ?>
-		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">Mediabit</a>
-		<?php else: ?>
-		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="http://www.zonales.com.ar/zonales-devel/templates/z1/swf/map_1.0.swf">Mediabit</a>
-		<?php endif; ?>
+		<?php echo $labelText; ?>
+	</p>
+	<?php endif; ?>
+	<p>
+		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">
+			<img src="templates/<?php echo $template; ?>/images/bot_zonales.gif" />
+		</a>
 	</p>
 </div>
