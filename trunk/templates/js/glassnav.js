ce7af19e56010db7a86ff5a2abb58ba32e7df59f
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-06-30 21:58:33 +0000

    Correcciones en el menú para mostrar/ocultar automaticamente los elementos del submenú.

--- trunk/templates/js/glassnav.js
+++ trunk/templates/js/glassnav.js
@@ -1,14 +1,16 @@
 window.addEvent('load', function(){
 
-		$('sublinks').getElements('ul').setStyle('display', 'none');
-		$('s1_m').setStyle('display', 'block');
+	// hide all submenu links
+	$('sublinks').getElements('ul').setStyle('display', 'none');
+	// show by default the first submenu
+	$('s0_m').setStyle('display', 'block');
 		
-		$$('#mymenu li').each(function(el){
-         el.getElement('a').addEvent('mouseover', function(subLinkId){
+	$$('#mymenu li').each(function(el){
+		el.getElement('a').addEvent('mouseover', function(subLinkId){
 			var layer = subLinkId+"_m";
-				$('sublinks').getElements('ul').setStyle('display', 'none');
-				$(layer).setStyle('display', 'block');
-          }.pass( el.id)
-        );      
-      });
-    });
+			$('sublinks').getElements('ul').setStyle('display', 'none');
+			$(layer).setStyle('display', 'block');
+		}.pass(el.id));
+	});
+
+});
