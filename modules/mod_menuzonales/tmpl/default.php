63effa66c7587523ae588065a56bb48d60f4a9d5
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-07-02 18:50:36 +0000

    Se carga el JS para el menú de zonales junto con el resto del componente.

--- modules/mod_menuzonales/tmpl/default.php
+++ modules/mod_menuzonales/tmpl/default.php
@@ -3,6 +3,27 @@
 defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
+<script type="text/javascript" defer="defer">
+	// <![CDATA[
+	window.addEvent('domready', function(){
+
+		// hide all submenu links
+		$('sublinks').getElements('ul').setStyle('display', 'none');
+		// show by default the first submenu
+		$('s0_m').setStyle('display', 'block');
+
+		$$('#mymenu li').each(function(el){
+			el.getElement('a').addEvent('mouseover', function(subLinkId){
+				var layer = subLinkId+"_m";
+				$('sublinks').getElements('ul').setStyle('display', 'none');
+				$(layer).setStyle('display', 'block');
+			}.pass(el.id));
+		});
+
+	});
+	// ]]>
+</script>
+
 <!-- glassmenu -->
 <div id="navigation">
 	<ul id="mymenu">
--- templates/component.php
+++ templates/component.php
@@ -13,7 +13,7 @@
 
 <script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/swfobject.js"></script>
 
-<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/glassnav.js"></script>
+<!-- <script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/glassnav.js"></script> -->
 <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/glassnav.css">
 
 
