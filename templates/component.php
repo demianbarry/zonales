10097470e4928bf1f28294dc338de15ad0394f1f
pastel83 <pastel83@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-07-06 17:57:36 +0000

    Arreglos para que valide XHTML 1.0 Transitional.

--- templates/component.php
+++ templates/component.php
@@ -21,7 +21,7 @@
 <center>
 <div id="wrapper">
 	<div id="top">
-		<div id="logoTop" style="float:left;"></div><!-- END #logoTop -->
+		<a href="index.php" style="cursor:crosshair;"><div id="logoTop" style="float:left;"></div><!-- END #logoTop --></a>
     
     <div style="float:right; padding-right:228px;">
     	<jdoc:include type="modules" name="topCenter" style="xhtml" />
@@ -76,6 +76,11 @@
   <div id="copy">
     Copyright 2009 ZONALES.COM.AR | Todos los derechos reservados.
   </div><!-- END #copy -->
+
+  <div id="w3c">
+	<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+	</div><!-- END #w3c -->
+
 </div><!-- END #wrapper -->
 </center>
 
