696db112e5d1b9083ff5af8c1c39fb9410c1ba78
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-07-16 17:04:33 +0000

    El zonal actual se representa mediante una imagen.

--- trunk/modules/mod_zonalactual/tmpl/default.php
+++ trunk/modules/mod_zonalactual/tmpl/default.php
@@ -1,12 +1,25 @@
 <?php
+/**
+ * @version	$Id$
+ * @package	Zonales
+ * @copyright	Copyright (C) 2009 Mediabit. All rights reserved.
+ * @license	GNU/GPL, see LICENSE.php
+ *
+ * Zonales is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
 <div id="moduletable_zName">
-    <p>
-	<?php if($showLabel) { echo $labelText; } ?>
-	&nbsp;
-	<?php echo $zonalName; ?>
-    </p>
+	<?php if ($showLabel): ?>
+	<p><?php echo $labelText; ?></p>
+	<?php endif; ?>
+	<img alt="<?php echo $zonalName; ?>" class="zonalactual" title="<?php echo $zonalName; ?>"
+	     src="templates/<?php echo $template; ?>/images/logozonal_<?php echo $zonal->name; ?>.gif" />
 </div>
