6fb85de42a140e54a8457860fb61136c6cbcbf2d
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-07-17 17:47:13 +0000

    Se asocia la noticia enviada por el usuario con los tags correspondientes al zonal y localidad seleccionados.

--- modules/mod_soycorresponsal/mod_soycorresponsal.php
+++ modules/mod_soycorresponsal/mod_soycorresponsal.php
@@ -32,14 +32,14 @@ $localidades = $helper->getFieldValues($zonal->id);
 
 // crea select de zonales disponibles
 $lists['partido_select'] = JHTML::_('select.genericlist', $zonales, 'partidos',
-	'size="1" class="required"', 'name', 'label', $zonal->name);
+	'size="1" class="required"', 'id', 'label', $zonal->id);
 
 // crea opcion nula para el select
 $blank_option[] = JHTML::_('select.option', '', JText::_('SELECCIONE_LOCALIDAD'), 'name', 'label');
 // crea select de zonales disponibles
 $localidades_list = array_merge($blank_option, $localidades);
 $lists['localidad_select'] = JHTML::_('select.genericlist', $localidades_list, 'localidad',
-	'size="1" class="required"', 'name', 'label');
+	'size="1" class="required"', 'id', 'label');
 
 // template
 $app =& JFactory::getApplication();
