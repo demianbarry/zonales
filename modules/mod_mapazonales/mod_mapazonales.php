3e68e6ff184847d8ef95db26151e6a210b0e8d61
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-07-20 20:17:40 +0000

    Selección de imagen adecuada según esquema de color seleccionado en el template.

--- modules/mod_mapazonales/mod_mapazonales.php
+++ modules/mod_mapazonales/mod_mapazonales.php
@@ -3,6 +3,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
+jimport ('joomla.filesystem.file');
 
 // parametros
 $showLabel = $params->get('show_label', 'show_label');
@@ -15,5 +16,12 @@ $useFlash = $params->get('use_flash', false);
 // template
 $app =& JFactory::getApplication();
 $template = $app->getTemplate();
+
+// valores para los parametros del template
+$tparams = new JParameter(JFile::read(JPATH_BASE.DS.'templates'.DS.
+		$template.DS.'params.ini'));
+
+$mainColor = $tparams->get('mainColor', null);
+if ($mainColor) $mainColor = '_' . $mainColor;
 
 require(JModuleHelper::getLayoutPath('mod_mapazonales'));
