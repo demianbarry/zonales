7d06b9a7bf2821e874eedd393b479767650eed01
g2patagonico <g2patagonico@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-08-21 22:58:57 +0000

    Pruebas de concepto con HTML + Javascript + Otros

---
+++ branches/pruebas/prueba1.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $salida = "<p>Reserva mesa ".$_GET['mesa']."</p>";
+    echo $salida;
+?>
