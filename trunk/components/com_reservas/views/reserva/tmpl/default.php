22518ad39e6b3e9830b19d98d3ca7338121c240d
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-09-10 12:26:03 +0000

    Ultimos cambios

--- trunk/components/com_reservas/views/reserva/tmpl/default.php
+++ trunk/components/com_reservas/views/reserva/tmpl/default.php
@@ -1,36 +1,30 @@
 <?php
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die ( 'Restricted Access' );
+
+// incluye javascript para objeto flash
+JHTML::script('dtfx.js');
 ?>
 
-<div id="seleccionaZ">
-<img src="templates/<?php echo $this->template; ?>/images/title_inicio.gif" border="0" alt="Zonalizate!"/>
-<div id="selZ-container">
-<div id="selZ-l">
-<img src="templates/<?php echo $this->template; ?>/images/title_inicio_seleccion.gif" border="0" alt="Seleccioná tu Zonal" />
-<div class="padd5">
-<p><?php echo JText::_('SELECCIONA_ZONAL'); ?></p>
-<form action="index.php" method="post" name="setZonalComForm" id="setZonalComForm">
-  <select class="cmb" id="selectZonal" name="selectZonal">
-    <?php foreach ($this->zonales as $zonal): ?>
-    <option value="<?php echo $zonal->id; ?>" <?php echo ($this->zonal_id == $zonal->id ? 'selected="selected"' : '') ?>><?php echo $zonal->label; ?></option>  
-    <?php endforeach; ?>
-  </select>
-  <input type="image" src="templates/<?php echo $this->template; ?>/images/bot_search_orange.gif" class="submit" name="button" title="title_here" />
-  <input type="hidden" name="task" value="setZonal" />
-  <input type="hidden" name="option" value="com_zonales" />
-  <?php echo JHTML::_('form.token'); ?>
-</form>
-</div><!-- end .padd5 -->
-</div><!-- end #selZ-l -->
-<div id="selZ-r">
-<!--
-<img src="templates/<?php echo $this->template; ?>/images/title_inicio_visitados.gif" border="0" alt="Los más visitados" />
-<div class="padd5">
-<a href="/zonales-devel/">Burzaco</a>
-<a href="/zonales-devel/">Adrogué</a>
-<a href="/zonales-devel/">Lomas de Zamora</a>
-</div><!-- end .padd5 -->  
-</div><!-- end #selZ-r -->
-</div><!-- end #selZ-container -->
-</div><!-- end #seleccionaZ -->
+<!--<div id="map_content">
+	Este DIV es reemplazado por el mapa flash utilizando el js SWFObject<br/>
+	Suele utilizarse este espacio para:
+	<ul>
+		<li>Avisar que aquí se cargará un mapa por ejemplo</li>
+		<li>Ofrecer una forma de instalar Flash Player</li>
+		<li>Ofrecer otra forma de elegir un zonal (por ejemplo, link a una página con un select)</li>
+	</ul>
+</div>-->
+<script>
+    javafx(
+    {
+        archive: "<?php echo $this->template; ?>/javafx/<?php echo $this->javafxfile; ?>",
+        draggable: <?php echo $this->draggable; ?>,
+        width: <?php echo $this->width; ?>,
+        height: <?php echo $this->height; ?>,
+        code: "<?php echo $this->code; ?>",
+        name: "<?php echo $this->name; ?>",
+        id: "<?php echo $this->code; ?>"
+    }
+);
+</script>
