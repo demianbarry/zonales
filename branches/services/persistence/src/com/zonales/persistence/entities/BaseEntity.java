350f6260e94f0e90759902d44c227ade429d3602
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-09-29 21:57:51 +0000

    Se agrega info de revisión y copyleft.

--- branches/services/persistence/src/com/zonales/persistence/entities/BaseEntity.java
+++ branches/services/persistence/src/com/zonales/persistence/entities/BaseEntity.java
@@ -1,8 +1,17 @@
+/**
+ * @version	$Id$
+ * @copyright	Copyright (C) 2009 Mediabit. All rights reserved.
+ * @license	GNU/GPL, see LICENSE.php
+ *
+ * Zonales is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 package com.zonales.persistence.entities;
 
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Properties;
 import javax.persistence.Column;
 import javax.persistence.Version;
 
