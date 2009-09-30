0024eda671e6f2266d788f9c7613ccc9fabac863
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-09-30 23:12:58 +0000

    Se crearon los models de cada entity

---
+++ branches/services/persistence/src/com/zonales/persistence/models/ValorPermitidoAtrcompModel.java
@@ -0,0 +1,25 @@
+/**
+ * @version	$Id$
+ * @copyright	Copyright (C) 2009 Mediabit. All rights reserved.
+ * @license	GNU/GPL, see LICENSE.php
+ *
+ * Zonales is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+package com.zonales.persistence.models;
+
+import com.zonales.persistence.entities.ValorPermitidoAtrcomp;
+
+/**
+ *
+ * @author g2p
+ */
+public class ValorPermitidoAtrcompModel extends BaseModel{
+    public ValorPermitidoAtrcompModel(){
+        super(ValorPermitidoAtrcomp.class);
+    }
+}
