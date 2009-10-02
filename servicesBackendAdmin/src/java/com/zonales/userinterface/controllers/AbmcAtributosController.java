d85f52f4288cc9d85233ef72111ec2c460858a6f
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-10-02 21:57:52 +0000

    Creando el proyecto de administración del backend de los servicios de zonales

---
+++ servicesBackendAdmin/src/java/com/zonales/userinterface/controllers/AbmcAtributosController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.zonales.userinterface.controllers;
+
+import com.zonales.persistence.entities.ClaseAtributo;
+import com.zonales.persistence.models.ClaseAtributoModel;
+import java.util.List;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zkplus.databind.DataBinder;
+import org.zkoss.zul.Grid;
+
+/**
+ *
+ * @author Cristian
+ */
+public class AbmcAtributosController extends BaseController{
+    private ClaseAtributoModel atributoModel;
+
+    private Grid atributos;
+
+    public AbmcAtributosController(){
+        super(true);
+        atributoModel = new ClaseAtributoModel();
+    }
+
+    public ClaseAtributoModel getAtributoModel() {
+        return atributoModel;
+    }
+
+    public void setAtributoModel(ClaseAtributoModel atributoModel) {
+        this.atributoModel = atributoModel;
+    }
+
+    public void onCreate$abmcAtributosWin(Event event) {
+        // Obtengo el DataBinder que instancia la página
+        binder = (DataBinder) getVariable("binder", true);
+
+//        final List model = (List) atributos.getModel();
+//
+//        if (!model.isEmpty()) {
+//            // si el model no está vacio, selecciono el primer registro y cargo el detalle
+//            atributoModel.setSelected((ClaseAtributo) model.get(0));
+//            binder.loadComponent(rolesDetail);
+//        }
+    }
+
+}
