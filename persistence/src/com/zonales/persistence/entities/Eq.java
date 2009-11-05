58a610e89d4fdcce798822ac299ce61b36ee6f69
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2009-11-05 18:05:07 +0000

    Se cambio de lazy a eager la forma de recuperación de la base de datos en las clases ClaseComponentee y Eq

--- persistence/src/com/zonales/persistence/entities/Eq.java
+++ persistence/src/com/zonales/persistence/entities/Eq.java
@@ -51,7 +51,7 @@ public class Eq extends BaseEntity implements Serializable {
     private Integer userId;
     @Column(name = "solrquery_bq")
     private String solrqueryBq;
-    @OneToMany(cascade = CascadeType.ALL, fetch=FetchType.LAZY, mappedBy = "eqId")
+    @OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER, mappedBy = "eqId")
     private List<EqAtributos> eqAtributosList;
 
     public Eq() {
