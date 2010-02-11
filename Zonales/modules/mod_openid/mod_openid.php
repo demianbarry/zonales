7916ddd572467f1262556e2e309ca0bce993d227
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-11 23:06:43 +0000

    Se agrego el modulo de OpenID

---
+++ Zonales/modules/mod_openid/mod_openid.php
@@ -0,0 +1,22 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+?>
+
+<form id="form-login" action="<?php echo JRoute::_('index.php'); ?>
+" method="post">
+    <fieldset class="input">
+        <p id="form-login-username">
+            <label for="modlgn_username">Ingrese su identificador OpenID:</label>
+            <br />
+            <input id="modlgn_username" class="inputbox" alt="username" name="username" size="18" type="text" />
+        </p>
+        <input name="option" type="hidden" value="com_user" />
+        <input name="task" type="hidden" value="login" />
+        <input name="return" type="hidden" value="L2luZGV4LnBocD9vcHRpb249Y29tX2NvbnRlbnQmdmlldz1mcm9udHBhZ2UmSXRlbWlkPTE=" />
+        <input name="provider" type="hidden" value="OpenID" />
+        <?php echo JHTML::_( 'form.token' ); ?>
+    </fieldset>
+</form>
+
+
