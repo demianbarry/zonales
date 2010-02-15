dd3525e055914733db50bb3de99009a142cc5986
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-15 22:56:32 +0000

    Se agregaron iconos de los proveedores Se modifico zlogin para que los distintos proveedores que implementan el mismo protocolo usen el mismo modulo

--- branches/services/Zonales/modules/mod_openid/mod_openid.php
+++ branches/services/Zonales/modules/mod_openid/mod_openid.php
@@ -7,14 +7,14 @@ jimport( 'joomla.methods' );
 <form id="form-openidlogin" action="<?php echo JRoute::_('index.php') ?>" method="post">
     <fieldset class="input">
         <p id="form-login-username">
-            <label for="modlgn_username">Ingrese su identificador OpenID:</label>
+            <label id="mod_login_message" name="mod_login_message" for="modlgn_username">Ingrese su identificador OpenID:</label>
             <br />
             <input id="modlgn_username" class="inputbox" alt="username" name="username" size="18" type="text" />
         </p>
         <input name="option" type="hidden" value="com_user" />
         <input name="task" type="hidden" value="login" />
         <input name="return" type="hidden" value="L2luZGV4LnBocD9vcHRpb249Y29tX2NvbnRlbnQmdmlldz1mcm9udHBhZ2UmSXRlbWlkPTE=" />
-        <input name="provider" type="hidden" value="OpenID" />
+        <input id="mod_login_provider" name="provider" type="hidden" value="OpenID" />
         <?php echo JHTML::_( 'form.token' ); ?>
     </fieldset>
 </form>
