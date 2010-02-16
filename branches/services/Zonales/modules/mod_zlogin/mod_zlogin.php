08c95fc8cc8f1c88d9ff05a202cfd524cfac00f8
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-16 00:24:41 +0000

    Se corrigieron errores en los modulos mod_openid y mod_zlogin

--- branches/services/Zonales/modules/mod_zlogin/mod_zlogin.php
+++ branches/services/Zonales/modules/mod_zlogin/mod_zlogin.php
@@ -15,26 +15,23 @@ $userislogged = (!$user->guest);
 ?>
 <script type="text/javascript">
     function setElement(id,message,provider){
-        document.getElementById(id + '_message').value=message;
-        document.getElementById(id + '_provider').value=provider;
+        if (id != 'mod_login'){
+            document.getElementById(id + '_message').innerHTML=message;
+            document.getElementById(id + '_provider').value=provider;
+        }
         showElement(id);
     }
 
     function showElement(id) {
 <?php
 foreach ($providerslist as $prov) {
-    if ($prov->module != null && !(!$user->guest && $prov->groupname == 'Guest')){
+    if ($prov->module != null && !(!$user->guest && $prov->groupname == 'Guest')) {
         echo 'document.getElementById(\'' . $prov->module . '\').style.display = \'none\';';
     }
 }
 ?>
-                document.getElementById(id).style.display = 'block';
-            }
-            <!--
-            Window.onDomReady(function(){
-                document.formvalidator.setHandler('passverify', function (value) { return ($('password').value == value); }	);
-            });
-            // -->
+        document.getElementById(id).style.display = 'block';
+    }
 </script>
 
 <table border="0" cellspacing="1">
@@ -45,40 +42,40 @@ foreach ($providerslist as $prov) {
     </thead>
     <tbody>
         <?php foreach ($providerslist as $provider): ?>
-        <?php if (!(!$user->guest && $provider->groupname == 'Guest')):
-        ?>
+            <?php if (!(!$user->guest && $provider->groupname == 'Guest')):
+                ?>
         <tr>
             <td>
                 <div>
 
-            <a href=<?php if ($provider->module != null) {
-                    echo '"#' .$provider->module. 'location" onClick="setElement(\''.$provider->module.'\')"';
-                }
-                else {
-                    $url = 'index.php?option=com_user&task=login&provider=' .
-                        $provider->name . '&' . JUtility::getToken() .'=1';
-                    echo '"' . $url . '"';
-                }
-                   ?>>
-                <img src="<?php echo $provider->icon_url ?>"
-                     alt="<?php echo $provider->name ?>"
-                     title="Ingrese a Zonales mediante <?php echo $provider->name ?>"
-                     />
-            </a>
-            <a name="<?php echo $provider->name ?>"></a>
-            <div style="display: none;" id="<?php echo $provider->module ?>">
-                <?php if ($provider->module != null) {
-                    $module = JModuleHelper::getModule($provider->module);
-                    $html = JModuleHelper::renderModule($module);
-                    echo $html;
-                }
-                ?>
-            </div>
+                    <a href=<?php if ($provider->module != null) {
+                                echo '"#' .$provider->module. 'location" onClick="setElement(\''.$provider->module.'\',\'Ingrese su identificacion de '.$provider->name.'\',\''.$provider->name.'\')"';
+                            }
+                            else {
+                                $url = 'index.php?option=com_user&task=login&provider=' .
+                                    $provider->name . '&' . JUtility::getToken() .'=1';
+                                echo '"' . $url . '"';
+                            }
+                               ?>>
+                        <img src="<?php echo $provider->icon_url ?>"
+                             alt="<?php echo $provider->name ?>"
+                             title="Ingrese a Zonales mediante <?php echo $provider->name ?>"
+                             />
+                    </a>
+                    <a name="<?php echo $provider->name ?>"></a>
+                    <div style="display: none;" id="<?php echo $provider->module ?>">
+                                <?php if ($provider->module != null) {
+                                    $module = JModuleHelper::getModule($provider->module);
+                                    $html = JModuleHelper::renderModule($module);
+                                    echo $html;
+                                }
+                                ?>
+                    </div>
 
-        </div>
+                </div>
             </td>
         </tr>
-        <?php endif ?>
+            <?php endif ?>
         <?php endforeach ?>
     </tbody>
 </table>
