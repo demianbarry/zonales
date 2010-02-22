b2c290988b3fb93bf0ed9bca19ed747269363a24
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-22 06:36:18 +0000

    Se creo creo un n nuevo controller para el zlogin

--- branches/services/Zonales/modules/mod_openid/mod_openid.php
+++ branches/services/Zonales/modules/mod_openid/mod_openid.php
@@ -5,24 +5,18 @@ jimport( 'joomla.methods' );
 $providerid = JRequest::getInt('providerid', '0', 'method');
 $externalid = JRequest::getVar('externalid', '', 'method', 'string');
 
+$message = JText::_('ZONALES_OPENID_ENTER_IDENTIFIER');
+
 ?>
 
-<form id="form-login" action="<?php echo JRoute::_('index.php') ?>" method="post">
-    <fieldset class="input">
+
+
         <p id="form-login-username">
-            <label id="mod_openid_message" name="mod_login_message" for="modlgn_username">Ingrese su identificador OpenID:</label>
+            <label id="mod_openid_message" name="mod_login_message" for="modlgn_username"><?php echo $message ?></label>
             <br />
             <input id="modlgn_username" class="inputbox" alt="username" name="username" size="18" type="text" />
         </p>
-        <input type="submit" name="Submit" class="button" value="<?php echo JText::_('LOGIN') ?>" />
-        <input name="option" type="hidden" value="com_user" />
-        <input name="task" type="hidden" value="login" />
-        <input name="return" type="hidden" value="<?php echo base64_encode(JRoute::_('index.php')) ?>" />
-        <input id="mod_openid_provider" name="provider" type="hidden" value="OpenID" />
-        <input type="hidden" name="providerid" value=<?php echo $providerid ?> />
-        <input type="hidden" name="externalid" value="<?php echo urlencode($externalid) ?>" />
-        <?php echo JHTML::_( 'form.token' ); ?>
-    </fieldset>
-</form>
+
+
 
 
