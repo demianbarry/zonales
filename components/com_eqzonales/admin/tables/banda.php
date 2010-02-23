1dc3883ac1744bfffa24b49971cb27b9f15110d0
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-23 19:21:18 +0000

    Componente Ecualizador para Zonales.
    Clases JTable y modelos necesarios para la interacción con la base de datos desde Joomla.

---
+++ components/com_eqzonales/admin/tables/banda.php
@@ -0,0 +1,41 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class TableBanda extends JTable
+{
+    /** @var int */
+    var $id             = null;
+    /** @var string */
+    var $valor          = null;
+    /** @var int */
+    var $peso           = null;
+    /** @var int */
+    var $cp_value_id    = null;
+    /** @var int */
+    var $eq_id          = null;
+    /** @var boolean */
+    var $default        = null;
+    /** @var boolean */
+    var $active         = null;
+
+    function __construct(&$db)
+    {
+        parent::__construct( '#__eqzonales_banda', 'id', $db );
+    }
+
+    /**
+     * Overloaded bind function
+     *
+     * @access public
+     * @return boolean
+     * @see JTable::bind
+     * @since 1.5
+     */
+    function bind( $from, $ignore=array() )
+    {
+        parent::bind($from, $ignore);
+
+        return true;
+    }
+
+}
