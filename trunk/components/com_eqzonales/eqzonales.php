c1b4203fdee5d7831571d61014a02a3b1785ae66
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-26 19:05:12 +0000

    Prueba de SVN properties.

--- trunk/components/com_eqzonales/eqzonales.php
+++ trunk/components/com_eqzonales/eqzonales.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version	$Id: user.php 10381 2008-06-01 03:35:53Z pasamio $
+* @version	$Id$
 * @package	Joomla
 * @subpackage	Users
 * @copyright	Copyright (C) 2005 - 2008. Mediabit. All rights reserved.
