793edbff4abd626de1fda717297393db8269bc8a
JuanManuelCortez <JuanManuelCortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-02-26 18:26:37 +0000

    - Controller por defecto del front end admite tagueo jerarquico

--- components/com_customproperties/site/controller.php
+++ components/com_customproperties/site/controller.php
@@ -1,14 +1,14 @@
 <?php
 /**
-* Custom Properties for Joomla! 1.5.x
-* @package Custom Properties
-* @subpackage Component
-* @version 1.98
-* @revision $Revision: 1.4 $
-* @author Andrea Forghieri
-* @copyright (C) Andrea Forghieri, www.solidsystem.it
-* @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL version 2
-*/
+ * Custom Properties for Joomla! 1.5.x
+ * @package Custom Properties
+ * @subpackage Component
+ * @version 1.98
+ * @revision $Revision: 1.4 $
+ * @author Andrea Forghieri
+ * @copyright (C) Andrea Forghieri, www.solidsystem.it
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL version 2
+ */
 
 
 // no direct access
@@ -25,115 +25,129 @@ jimport('joomla.application.component.controller');
  */
 class CustompropertiesController extends JController {
 
-	/**
-	 * constructor (registers additional tasks to methods)
-	 * @return void
-	 */
-	function __construct() {
-		parent::__construct();
-		$this->registerTask('add', 		'assignCP');
-		$this->registerTask('replace',	'assignCP');
-	}
+    /**
+     * constructor (registers additional tasks to methods)
+     * @return void
+     */
+    function __construct() {
+        parent::__construct();
+        $this->registerTask('add', 		'assignCP');
+        $this->registerTask('replace',	'assignCP');
+    }
 
-	/**
-	 * Method to display the view
-	 *
-	 * @access    public
-	 */
-	function display() {
+    /**
+     * Method to display the view
+     *
+     * @access    public
+     */
+    function display() {
 
-		$vName = JRequest::getCmd('view', 'show');
-		$document = & JFactory::getDocument();
-		$vType = $document->getType();
+        $vName = JRequest::getCmd('view', 'show');
+        $document = & JFactory::getDocument();
+        $vType = $document->getType();
 
-		switch ($vName) {
-			case 'tagging' :
+        switch ($vName) {
+            case 'tagging' :
 
-				$document->addStyleSheet('components/com_customproperties/css/customproperties.css');
-				$vName = 'tagging';
-				$vLayout = JRequest::getCmd('layout', 'default');
-				// add dministrator views
-				$this->addViewPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'views' . DS);
-				$view = & $this->getView($vName, $vType);
-				//add administrators models
-				$this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'models' . DS);
-				$view->setModel($this->getModel('assign', 'CustompropertiesModel'));
-				$view->setModel($this->getModel('cpfields', 'CustompropertiesModel'));
+                $document->addStyleSheet('components/com_customproperties/css/customproperties.css');
+                $vName = 'tagging';
+                $vLayout = JRequest::getCmd('layout', 'default');
+                // add dministrator views
+                $this->addViewPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'views' . DS);
+                $view = & $this->getView($vName, $vType);
+                //add administrators models
+                $this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'models' . DS);
+                $view->setModel($this->getModel('assign', 'CustompropertiesModel'));
+                $view->setModel($this->getModel('cpfields', 'CustompropertiesModel'));
 
-				break;
-			case 'show' :
-			default :
-				$view 		=& $this->getView($vName, $vType);
-				$vLayout 	= JRequest::getCmd('layout', 'default');
-				$view->setModel($this->getModel('search', 'CustompropertiesModel'), true);
-				break;
-		}
+                break;
+            case 'hierarchictagging' :
 
-		// Get/Create the view
-		$view->addTemplatePath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'views' . DS . strtolower($vName) . DS . 'tmpl');
+                $document->addStyleSheet('components/com_customproperties/css/customproperties.css');
+                $vName = 'hierarchictagging';
+                $vLayout = JRequest::getCmd('layout', 'default');
+                // add dministrator views
+                $this->addViewPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'views' . DS);
+                $view = & $this->getView($vName, $vType);
+                //add administrators models
+                $this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'models' . DS);
+                $view->setModel($this->getModel('assignhierarchic', 'CustompropertiesModel'));
+                $view->setModel($this->getModel('cpvalues', 'CustompropertiesModel'));
 
-		// Set the layout
-		$view->setLayout($vLayout);
+                break;
+            case 'show' :
+            default :
+                $view 		=& $this->getView($vName, $vType);
+                $vLayout 	= JRequest::getCmd('layout', 'default');
+                $view->setModel($this->getModel('search', 'CustompropertiesModel'), true);
+                break;
+        }
 
-		// Display the view
-		$view->display();
+        // Get/Create the view
+        $view->addTemplatePath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'views' . DS . strtolower($vName) . DS . 'tmpl');
 
-	}
+        // Set the layout
+        $view->setLayout($vLayout);
 
-	/** Method to assign custom properties from the frontend
-	 *
-	 * @returns void
-	 */
-	function assignCP() {
-		/*
+        // Display the view
+        $view->display();
+
+    }
+
+    /** Method to assign custom properties from the frontend
+     *
+     * @returns void
+     */
+    function assignCP() {
+        /*
 		 * TODO This function is a verbatim copy of the function in backend's controller
 		 * thus it is not DRY compliant :(
 		 * sooner or later I'll fix it'
-		 */
+        */
 
-		global $cp_config;
-		require_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'cp_config.php');
+        global $cp_config;
+        require_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'cp_config.php');
 
-		$this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'models' . DS);
-		$model = $this->getModel('assign', 'CustompropertiesModel');
+        $this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'models' . DS);
+        $model = $this->getModel('assign', 'CustompropertiesModel');
 
-		$content_id = JRequest::getVar('cid', 0, '', 'int');
-		$database 	= JFactory::getDBO();
-		$user 		= JFactory::getUser();
+        $content_id = JRequest::getVar('cid', 0, '', 'int');
+        $database 	= JFactory::getDBO();
+        $user 		= JFactory::getUser();
 
-		switch ($this->getTask()) {
-			case 'add' :
-				$action = 'add';
-				break;
-			case 'replace' :
-				$action = 'replace';
-				break;
-			default :
-				return;
-		}
+        switch ($this->getTask()) {
+            case 'add' :
+                $action = 'add';
+                break;
+            case 'replace' :
+                $action = 'replace';
+                break;
+            default :
+                return;
+        }
 
-		$option 	= JRequest::getCmd('option'		, 'com_customproperties');
-		$controller = JRequest::getCmd('controller'	, 'tagging');
-		$view 		= JRequest::getCmd('view'		, 'tagging');
+        $option 	= JRequest::getCmd('option'		, 'com_customproperties');
+        $controller = JRequest::getCmd('controller'	, 'tagging');
+        $view 		= JRequest::getCmd('view'		, 'tagging');
 
-		$return_to = "index.php?option=$option&controller=$controller&view=$view&tmpl=component&cid=$content_id";
+        $return_to = "index.php?option=$option&controller=$controller&view=$view&tmpl=component_only&cid=$content_id";
 
-		if ($content_id === 0) {
-			$this->setRedirect($return_to, JText::_('CP_ERR_INVALID_ID'), 'error');
-			return;
-		};
-		if ($cp_config['frontend_tagging'] != 1) {
-			$this->setRedirect($return_to, JText::_('CP_ERR_FUNCTION_DISABLED'), 'error');
-			return;
-		}
-		if ($user->get('gid') < $cp_config['editing_level']) {
-			$this->setRedirect($return_to, JText::_('CP_ERR_NOAUTH'), 'error');
-			return;
-		}
+        if ($content_id === 0) {
+            $this->setRedirect($return_to, JText::_('CP_ERR_INVALID_ID'), 'error');
+            return;
+        };
+        if ($cp_config['frontend_tagging'] != 1) {
+            $this->setRedirect($return_to, JText::_('CP_ERR_FUNCTION_DISABLED'), 'error');
+            return;
+        }
+        if ($user->get('gid') < $cp_config['editing_level']) {
+            $this->setRedirect($return_to, JText::_('CP_ERR_NOAUTH'), 'error');
+            return;
+        }
 
-		$model->assignCustomProperties($action);
-		$this->setRedirect($return_to);
+        $model->assignCustomProperties($action);
+        $this->setRedirect($return_to);
 
-	}
+    }
 
 }
