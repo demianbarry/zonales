6656e76e0086d7982c3c2acc797de2913f469bb3
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-03-04 22:16:15 +0000

    Se modifica el pop-up con leyendas y avisos en español.

--- trunk/modules/mod_mapazonales/tmpl/default.php
+++ trunk/modules/mod_mapazonales/tmpl/default.php
@@ -4,29 +4,41 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 ?>
 
 <script type="text/javascript" defer="defer">
-	// <![CDATA[
-	window.addEvent('domready', function() {
-		Shadowbox.init({
-			skipSetup: true
-		});
+    // <![CDATA[
+    window.addEvent('domready', function() {
+        Shadowbox.init({
+            skipSetup: true,
+            text: {
+                cancel:     'Cancelar',
+                loading:    'Cargando...',
+                close:      '<span class="shortcut">C</span>errar',
+                next:       '<span class="shortcut">S</span>iguiente',
+                prev:       '<span class="shortcut">A</span>nterior',
+                errors:     {
+                    single: 'Usted debe instalar el plugin <a href="{0}">{1}</a> para poder ver el contenido.',
+                    shared: 'Usted debe instalar <a href="{0}">{1}</a> y <a href="{2}">{3}</a> para poder visualizar el contenido.',
+                    either: 'Usted debe instalar <a href="{0}">{1}</a> o <a href="{2}">{3}</a> para poder visualizar el contenido.'
+                }
+            }
+        });
 
-		Shadowbox.setup($('zonal'), {
-			onClose:function() { window.location.reload(true); }
-		});
-	});
+        Shadowbox.setup($('zonal'), {
+            onClose:function() { window.location.reload(true); }
+        });
+    });
 
-	// ]]>
+    // ]]>
 </script>
 
 <div id="moduletable_zMap">
-	<?php if($showLabel): ?>
-	<p>
-		<?php echo $labelText; ?>
-	</p>
-	<?php endif; ?>
-	<p>
-		<a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">
-			<img src="templates/<?php echo $template; ?>/images/<?php echo $mainColor; ?>/bot_zonales.gif" />
-		</a>
-	</p>
+    <?php if($showLabel): ?>
+    <p>
+            <?php echo $labelText; ?>
+    </p>
+    <?php endif; ?>
+    <p>
+        <a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">
+            <img src="templates/<?php echo $template; ?>/images/<?php echo $mainColor; ?>/bot_zonales.gif" />
+        </a>
+    </p>
 </div>
