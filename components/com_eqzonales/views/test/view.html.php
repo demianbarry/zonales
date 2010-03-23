1d2efebcce19cfa747f120c986ab8e3924cec5bc
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-03-23 20:16:13 +0000

    Se implementaron funcionalidades de la API del motor de ecualización:
    * Creación de EQ
    * Modificación de EQ
    * Creación/Modificación de las bandas

---
+++ components/com_eqzonales/views/test/view.html.php
@@ -0,0 +1,23 @@
+<?php
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * User component login view class
+ *
+ * @package		Joomla
+ * @subpackage	Users
+ * @since	1.0
+ */
+class EqZonalesViewTest extends JView
+{
+	function display($tpl = null)
+	{
+		global $mainframe, $option;
+
+		parent::display($tpl);
+	}
+}
