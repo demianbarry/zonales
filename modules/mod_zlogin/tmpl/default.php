172a60bb8375328e2aef011d3b1c59b206174aed
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-04-07 19:42:21 +0000

    Se agrego propiedad Id.

--- modules/mod_zlogin/tmpl/default.php
+++ modules/mod_zlogin/tmpl/default.php
@@ -1,3 +1,21 @@
+<?php
+/**
+ * @version	$Id$
+ * @package	Zonales
+ * @copyright	Copyright (C) 2009 Mediabit. All rights reserved.
+ * @license	GNU/GPL, see LICENSE.php
+ *
+ * Zonales is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die ( 'Restricted Access' );
+?>
+
 <!-- no tocar este display -->
 <div id="zlogin"
      style="display: <?php echo ($userislogged) ? 'none' : 'block' ?>;">
@@ -143,7 +161,3 @@
         </fieldset>
     </form>
 </div>
-
-<!-- <input type="button" onclick="FB.Connect.requireSession(); return false;" value="IR"></input> -->
-<!-- <a href="#" onClick="FB.Connect.logout(function() { refresh_page(); })">Logout of Facebook</a> -->
-
