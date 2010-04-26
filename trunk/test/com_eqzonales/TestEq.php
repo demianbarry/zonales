4e382d2fe6d952b92821e59735d372772b4c65b4
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-04-26 16:36:15 +0000

    Configuración por medio de archivo ini.

--- trunk/test/com_eqzonales/TestEq.php
+++ trunk/test/com_eqzonales/TestEq.php
@@ -3,13 +3,30 @@
 class TestEq extends UnitTestCase {
 
     var $r;
+    var $cookieName = 'cookieName';
+    var $cookieValue = 'cookieValue';
+    var $userhttp = 'userName';
+    var $passhttp = 'password';
+    var $host = 'host';
+    var $port = 1234;
+    var $db = 'db';
+    var $userdb = 'userdb';
+    var $passdb = 'passdb';
 
     function TestEq() {
         $this->UnitTestCase('TestEq');
     }
 
     function setUp() {
-        $r = new HttpRequest('http://www.zonales.com.ar:50080/', HttpRequest::METH_POST);
+        $this->r = new HttpRequest('http://www.zonales.com.ar:50080/', HttpRequest::METH_POST);
+        $ini_array = parse_ini_file("conf.ini");
+        $this->userhttp = $ini_array['userhttp'];
+        $this->passhttp = $ini_array['passhttp'];
+        $this->host = $ini_array['host'];
+        $this->port = $ini_array['port'];
+        $this->db = $ini_array['db'];
+        $this->userdb = $ini_array['userdb'];
+        $this->passdb = $ini_array['passdb'];
     }
 
     function tearDown() {
@@ -26,8 +43,8 @@ class TestEq extends UnitTestCase {
                 'params' => json_encode($json_msg)
         );
 
-        $r = new HttpRequest('http://user:pass@www.zonales.com.ar:50080/', HttpRequest::METH_POST);
-        $r->addCookies(array('name' => 'value'));
+        $r = new HttpRequest("http://$userhttp:$passhttp@www.zonales.com.ar:50080/", HttpRequest::METH_POST);
+        $r->addCookies(array("$cookieName" => "$cookieValue"));
         $r->addPostFields($params);
 
         try {
@@ -35,38 +52,73 @@ class TestEq extends UnitTestCase {
             $this->assertNotNull($resp);
             $json_resp = json_decode($resp);
             if ($this->assertNotNull($json_resp)) {
-                $this->assertEqual("SUCCESS", $json_resp->status);
-            }
+                if ($this->assertEqual("SUCCESS", $json_resp->status)) {
 
-            $con = mysql_connect("host:puerto","usuario","password");
-            if (!$con) {
-                die('No se pudo conectar: ' . mysql_error());
+                    $con = mysql_connect("$host:$port", $userdb, $passdb);
+                    if (!$con) {
+                        die('No se pudo conectar: ' . mysql_error());
+                    }
+                    mysql_select_db($db, $con);
+                    if (!$db_selected) {
+                        die ('No se puede conectar a la base de datos : ' . mysql_error());
+                    }
+                    $query = mysql_query("SELECT count(*) AS c FROM jos_eqzonales_eq t WHERE t.nombre = 'TEST1'");
+                    if (!$query) {
+                        die('Consulta inválida: ' . mysql_error());
+                    }
+                    $result = mysql_fetch_array($query);
+                    $this->assertEqual(1, intval($result[0]));
+                    mysql_close($con);
+                }
             }
-            mysql_select_db("db", $con);
-            $result = mysql_query("SELECT count(*) AS c FROM jos_eqzonales_eq t WHERE t.nombre = 'TEST1'");
-            $this->assetEqual(1, $result['c']);
-            mysql_close($con);
-        } catch (HttpException $ex) {
-
-        }
+        } catch (HttpException $ex) { }
     }
 
-    function estFallaCreaNuevoEcualizador() {
-        $json_msg = array("user_id" => 1, "nombre_eq" => "TEST1");
+    function testFallaCreaNuevoEcualizadorUsuarioIncorrecto() {
+        $json_msg = array("user_id" => -1, "nombre" => "TEST1");
 
         $params = array(
                 'task' => 'eq.createeq',
+                'format' => 'raw',
+                'option' => 'com_eqzonales',
                 'params' => json_encode($json_msg)
         );
 
+        $r = new HttpRequest("http://$userhttp:$passhttp@www.zonales.com.ar:50080/", HttpRequest::METH_POST);
+        $r->addCookies(array("$cookieName" => "$cookieValue"));
         $r->addPostFields($params);
 
         try {
-            $resp = json_decode($r->send()->getBody());
-            assertFalse($resp);
-        } catch (HttpException $ex) {
+            $resp = $r->send()->getBody();
+            $this->assertNotNull($resp);
+            $json_resp = json_decode($resp);
+            if ($this->assertNotNull($json_resp)) {
+                $this->assertEqual("FAILURE", $json_resp->status);
+            }
+        } catch (HttpException $ex) { }
+    }
 
-        }
+    function testFallaCreaNuevoEcualizadorUsuarioGuest() {
+        $json_msg = array("user_id" => 0, "nombre" => "TEST1");
+
+        $params = array(
+                'task' => 'eq.createeq',
+                'format' => 'raw',
+                'option' => 'com_eqzonales',
+                'params' => json_encode($json_msg)
+        );
+
+        $r = new HttpRequest("http://$userhttp:$passhttp@www.zonales.com.ar:50080/", HttpRequest::METH_POST);
+        $r->addPostFields($params);
+
+        try {
+            $resp = $r->send()->getBody();
+            $this->assertNotNull($resp);
+            $json_resp = json_decode($resp);
+            if ($this->assertNotNull($json_resp)) {
+                $this->assertEqual("FAILURE", $json_resp->status);
+            }
+        } catch (HttpException $ex) { }
     }
 
     function testModificaEcualizador() {
