2748bbeb72bde863e0295970000bce57ea9d42f7
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-06 22:19:49 +0000

    Correcciones varias en renders, validators, etc.

---
+++ trunk/components/com_aapu/admin/plugins/renders/render_for_MULTI_LISTBOX_data_type.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+*/
+
+/**
+ * Description of render_for_TEXT_data_type
+ *
+ * @author nacho
+ */
+class render_for_MULTI_LISTBOX_data_type {
+    //put your code here
+
+   function render($id, $value = null, $label = null, $required = null, $params = null) {
+
+        $req = $required == null ? '' : $required == 0 ? '' : 'required';
+        $req == 'required' ? $reqLabel = '*' : $reqLabel = '';
+        $value = explode(',', $value);
+
+        if (!is_array($params['values_list'])) {
+            $values = explode(',',$params['values_list']);
+            foreach ($values as $k => $v) {
+                $valuesList[$k] = new stdClass();
+                $valuesList[$k]->key = $v;
+                $valuesList[$k]->value = $v;
+            }
+        } else {
+            $values = $params['values_list'];
+            foreach ($values as $k => $v) {
+                $valuesList[$k] = new stdClass();
+                $valuesList[$k]->key = $v[0];
+                $valuesList[$k]->value = $v[1];
+            }
+        }
+
+        $return =
+            '<div class="render">
+                 <td class="key">
+                    <label>'.$label.' '.$reqLabel.'</label>
+                 </td>
+                 <td colspan="2">'.
+                    JHTML::_('select.genericlist', $valuesList, 'attr_'.$id.'[]', 'multiple="multiple" class="listbox" size="5"', 'key', 'value', $value )
+                    .'<div id="valid_'.$id.'" style="float: right; color: red"></div>
+                </td>
+            </div>';
+
+        return $return;
+    }
+
+    //Retorna donde se guarda el dato: value, value_int, value_double, value_date, value_boolean
+    function getPrimitiveType() {
+        return 'value';
+    }
+
+}
+?>
+
