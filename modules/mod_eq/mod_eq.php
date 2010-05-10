b86c70e955621d610051a45536bdf65e078126a1
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-10 19:36:22 +0000

    GUI e i18n del ecualizador de interes.

--- modules/mod_eq/mod_eq.php
+++ modules/mod_eq/mod_eq.php
@@ -5,7 +5,11 @@ defined('_JEXEC') or die('Restricted access');
 //require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'eq.php');
 require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'band.php');
+require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'helper'.DS.'helper.php');
 JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'tables');
+
+// helper
+$helper = new comEqZonalesHelper();
 
 // controladores
 $ctrlEq = new EqZonalesControllerEq();
@@ -23,7 +27,14 @@ $description = $params->get('description');
 // recupera el usuario
 $user =& JFactory::getUser();
 
-// recupera ecualizador del usuario
-$eq = $ctrlEq->retrieveUserEqImpl($user->id);
+$eq = null;
+
+if (!$user->guest) {
+    // recupera ecualizador del usuario
+    $result = $ctrlEq->retrieveUserEqImpl($user->id);
+    if (!is_null($result) && !empty($result)) {
+        $eq = $result[0];
+    }
+}
 
 require(JModuleHelper::getLayoutPath('mod_eq'));
