9473cf0b35ef0f3517eba9135436a46b2259d5e4
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-15 14:51:48 +0000

    Módulo Ecualizador de Interés.

--- modules/mod_eq/mod_eq.php
+++ modules/mod_eq/mod_eq.php
@@ -23,6 +23,7 @@ $template = $app->getTemplate();
 
 // parametros
 $description = $params->get('description');
+$error_no_eq = $params->get('error_no_eq');
 
 // recupera el usuario
 $user =& JFactory::getUser();
@@ -33,8 +34,29 @@ if (!$user->guest) {
     // recupera ecualizador del usuario
     $result = $ctrlEq->retrieveUserEqImpl($user->id);
     if (!is_null($result) && !empty($result)) {
-        $eq = $result[0];
+        $eqtmp = $result[0];
+
+        $eq->eq = $eqtmp->eq;
+
+        // segmentamos por fields (grupos de bandas)
+        foreach ($eqtmp->bands as $band) {
+            $eq->fields[$band->field_id]->id = $band->field_id;
+            $eq->fields[$band->field_id]->label = $band->group_label;
+            $eq->fields[$band->field_id]->bands[] = $band;
+        }
     }
 }
+
+$addTagsUrl = JRoute::_('index.php?option=com_eqzonales&task=hierarchictagging.display');
+$selectTags = 'select v.name as value from jos_custom_properties cp, jos_custom_properties_values v where cp.value_id=v.id';
+$db = JFactory::getDBO();
+$db->setQuery($selectTags);
+$dbTags = $db->loadObjectList();
+
+$aux = array();
+foreach ($dbTags as $tag) {
+    $aux[] = $tag->value;
+}
+$tags = implode(', ', $aux);
 
 require(JModuleHelper::getLayoutPath('mod_eq'));
