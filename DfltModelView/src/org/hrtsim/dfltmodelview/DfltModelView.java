a99532b96f4828052d3c3b7a8808b2373089fec3
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-18 12:40:16 +0000

    Implementación por defecto de visualizador de modelo de simulación.

---
+++ DfltModelView/src/org/hrtsim/dfltmodelview/DfltModelView.java
@@ -0,0 +1,20 @@
+package org.hrtsim.dfltmodelview;
+
+import org.hrtsim.dfltmodelview.impl.DfltModelViewSupport;
+import org.hrtsim.modelview.api.ModelView;
+import org.openide.loaders.MultiDataObject;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.windows.CloneableOpenSupport;
+
+/**
+ *
+ * @author fep
+ */
+@ServiceProvider(service = ModelView.class)
+public class DfltModelView implements ModelView {
+
+    public CloneableOpenSupport getOpenSupport(MultiDataObject mdo) {
+        return new DfltModelViewSupport(mdo.getPrimaryEntry());
+    }
+
+}
---
+++ DfltModelView/src/org/hrtsim/dfltmodelview/impl/DfltModelViewSupport.java
@@ -0,0 +1,26 @@
+package org.hrtsim.dfltmodelview.impl;
+
+import org.openide.cookies.CloseCookie;
+import org.openide.cookies.OpenCookie;
+import org.openide.loaders.MultiDataObject;
+import org.openide.loaders.OpenSupport;
+import org.openide.windows.CloneableTopComponent;
+
+/**
+ *
+ * @author fep
+ */
+public class DfltModelViewSupport extends OpenSupport implements OpenCookie, CloseCookie {
+
+    public DfltModelViewSupport(MultiDataObject.Entry e) {
+        super(e);
+    }
+
+    @Override
+    protected CloneableTopComponent createCloneableTopComponent() {
+        DfltModelViewTopComponent tc = new DfltModelViewTopComponent();
+        tc.setDisplayName("Default");
+        return tc;
+    }
+    
+}
