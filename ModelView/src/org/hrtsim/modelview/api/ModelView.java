0ba5ff322997ad50ee1ff6e35661ad6dd46eb369
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-18 12:38:38 +0000

    Componente con definición base de visualización del modelo a simular.

---
+++ ModelView/src/org/hrtsim/modelview/api/ModelView.java
@@ -0,0 +1,14 @@
+package org.hrtsim.modelview.api;
+
+import org.openide.loaders.MultiDataObject;
+import org.openide.windows.CloneableOpenSupport;
+
+/**
+ *
+ * @author fep
+ */
+public interface ModelView  {
+
+    public CloneableOpenSupport getOpenSupport(MultiDataObject mdo);
+
+}
