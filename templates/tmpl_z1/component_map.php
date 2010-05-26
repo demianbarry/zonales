6919d4ed5ec9f8e54179403000b2975b3faee285
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-26 21:46:02 +0000

    Se modifico el component_map.php del template para que funcione con facebook

--- templates/tmpl_z1/component_map.php
+++ templates/tmpl_z1/component_map.php
@@ -1,6 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" xmlns:fb="http://www.facebook.com/2008/fbml" >
 <head>
 <jdoc:include type="head" />
 
@@ -30,6 +30,13 @@
 	<a href="http://validator.w3.org/check?uri=referer"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/images/w3c_xhtml_01.gif" alt="Valid XHTML 1.0 Transitional" /></a>
 </div><!-- END #w3c -->
 
-
+<?php
+    $user =& JFactory::getUser();
+    if ($user->guest):
+?>
+    <script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/es_LA"></script>
+    <script type="text/javascript">  FB.init("91778705a13235cd3efe59d31e4d31bf","xd_receiver.htm");</script>
+<!-- <script type="text/javascript">  FB.init("<?php //echo $this->apikey ?>","xd_receiver.htm");</script> -->
+<?php endif ?>
 </body>
 </html>
