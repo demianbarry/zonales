1a1bc9ec4343e432a2b4fc6c116ec8d3710dc379
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-28 20:08:54 +0000

    Actualización plug-in de búsqueda

--- plugins/phpsolrsearch/solrsearch.php
+++ plugins/phpsolrsearch/solrsearch.php
@@ -43,6 +43,7 @@ class plgSearchSolrsearch extends JPlugin {
     function onSearch($text, $phrase='', $ordering='', $areas=null) {
         require_once(JPATH_SITE.DS.'components'.DS.'com_content'.DS.'helpers'.DS.'route.php');
         require_once(JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_search'.DS.'helpers'.DS.'search.php');
+        require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'helper'.DS.'contenthelper.php');
 
         $return = array();
 
@@ -76,80 +77,8 @@ class plgSearchSolrsearch extends JPlugin {
         $sUncategorised = $pluginParams->get( 'search_uncategorised', 1 );
         $sArchived = $pluginParams->get( 'search_archived', 1 );
         $limit = $pluginParams->get( 'search_limit', 50 );
-        $solr_url = $pluginParams->get( 'solr_url', null );
-        $solr_port = $pluginParams->get( 'solr_port', null );
-        $solr_webapp = $pluginParams->get( 'solr_webapp', null );
-
-        // No se especifico la url de solr
-        if ($solr_url == null) {
-            return array();
-        }
-        // No se especifico el puerto de solr
-        if ($solr_port == null) {
-            return array();
-        }
-        // No se especifico el puerto de webapp
-        if ($solr_webapp == null) {
-            return array();
-        }
-
-        // The Apache Solr Client library should be on the include path
-        // which is usually most easily accomplished by placing in the
-        // same directory as this script ( . or current directory is a default
-        // php include path entry in the php.ini)
-        jimport('SolrPhpClient.Apache.Solr.Service');
-
-        // create a new solr service instance - host, port, and webapp
-        // path (all defaults in this example)
-        $solr = new Apache_Solr_Service($solr_url, $solr_port, $solr_webapp);
-
-        $additionalParameters = array(
-                'published' => 'true',
-                'section_published' => 'true',
-                'category_published' => 'true',
-                'hasPublishUpDate' => 'false',
-                'publishUpDate' => '[* TO NOW])',
-                'hasPublishDownDate' => 'false',
-                'publishDownDate' => '[NOW TO *])',
-                'a_access' => '[* TO ' . $user->aid . ']',
-                'section_access' => '[* TO ' + $user->aid + ']',
-                'category_access' => '[* TO ' + $user->aid + ']'
-        );
-
-        /**
-         * TODO: Parametrizar en el backend que campo en el esquema de solr
-         * que deberá utilizarse para cada tipo de ordenamiento, y así envíar
-         * en el resquest unicamente el concepto (ej, 'oldest') y el orden.
-         */
-        switch ($ordering) {
-            case 'oldest':
-                $additionalParameters['sort'] = 'creationDate asc';
-                break;
-
-            case 'popular':
-                $additionalParameters['sort'] = 'hits desc';
-                break;
-
-            case 'alpha':
-                $additionalParameters['sort'] = 'title_s asc';
-                break;
-
-            case 'category':
-                $additionalParameters['sort'] = 'category_s asc';
-                break;
-
-            case 'newest':
-            default:
-                $additionalParameters['sort'] = 'creationDate desc';
-                break;
-        }
-
-        try {
-            $results = $solr->search($text, 0, $limit, $additionalParameters);
-        }
-        catch (Exception $e) {
-            return array();
-        }
+       
+        $results = $this->getSolrResults(0, $limit);
 
         // display results
         if ($results) {
@@ -170,7 +99,7 @@ class plgSearchSolrsearch extends JPlugin {
                     $item->section = JText::_('Uncategorised Content');
                 }
 
-                $item->created = $doc->creationDate;
+                $item->created = $doc->created;
                 $item->text = $doc->intro_content;
                 $item->browsernav = '';
                 $return[] = $item;
@@ -180,4 +109,189 @@ class plgSearchSolrsearch extends JPlugin {
 
         return $return;
     }
+
+    /**
+     *
+     * @param <type> $limit
+     * @param <type> $limitstart
+     * @param <type> $queryParams
+     * @return <type>
+     */
+    function getSolrResults($limitstart = 0, $limit = 0, $additionalParams = '') {
+
+        $zonalesParams = &JComponentHelper::getParams( 'com_eqzonales' );
+
+        // recupera parametros
+        $solr_url = $zonalesParams->get( 'solr_url', null );
+        $solr_port = $zonalesParams->get( 'solr_port', null );
+        $solr_webapp = $zonalesParams->get( 'solr_webapp', null );
+
+        // No se especifico la url de solr
+        if ($solr_url == null) {
+            return null;
+        }
+        // No se especifico el puerto de solr
+        if ($solr_port == null) {
+            return null;
+        }
+        // No se especifico el puerto de webapp
+        if ($solr_webapp == null) {
+            return null;
+        }
+
+        // The Apache Solr Client library should be on the include path
+        // which is usuaPlly most easily accomplished by placing in the
+        // same directory as this script ( . or current directory is a default
+        // php include path entry in the php.ini)
+        jimport('SolrPhpClient.Apache.Solr.Service');
+
+        // create a new solr service instance - host, port, and webapp
+        // path (all defaults in this example)
+        $solr = new Apache_Solr_Service($solr_url, $solr_port, $solr_webapp);
+
+        $queryParams = array();
+
+        $fqParams = array();
+
+        $fqParams[] = $this->getWhere();
+
+        if (strlen($additionalParams) > 0) {
+            $fqParams[] = $additionalParams;
+        }
+
+        $queryParams['fq'] = $fqParams;
+        $queryParams['sort'] = $this->getOrder();
+        $queryParams['fl'] = $this->getFieldList();
+        $queryParams['bq'] = $this->getEqPreferences();
+        $queryParams['qt'] = "zonalesContent";
+
+        try {
+            $results = $solr->search("", $limitstart, $limit, $queryParams);
+        }
+        catch (Exception $e) {
+            return null;
+        }
+
+        return $results;
+    }
+
+    /**
+     *
+     *
+     * @return <type>
+     */
+    function getFieldList() {
+        $fieldList =
+                "category,id,title,alias,title_alias,introtext,fulltext,sectionid,".
+                "state,catid,created,created_by,created_by_alias,modified,modified_by,".
+                "checked_out,checked_out_time,publish_up,publish_down,attribs,hits,".
+                "images,urls,ordering,metakey,metadesc,access,slug,catslug,readmore,".
+                "author,usertype,groups,author_email";
+
+        return $fieldList;
+    }
+
+    /**
+     *
+     *
+     * @return <type>
+     */
+    function getOrder() {
+        // Get the page/component configuration
+        $orderby = "score desc,created desc";
+        return $orderby;
+    }
+
+    /**
+     *
+     * @global <type> $mainframe
+     * @param <type> $additionalParams
+     * @return <type>
+     */
+    function getWhere() {
+        global $mainframe;
+
+        // usuario
+        $user =& JFactory::getUser();
+        $gid = $user->get('aid', 0);
+
+        // fecha
+        $jnow =& JFactory::getDate();
+        $now = $jnow->toMySQL();
+
+        // Get the page/component configuration
+        $params = &$mainframe->getParams();
+        $noauth = !$params->get('show_noauth');
+        //$nullDate = $this->_db->getNullDate();
+
+        $where = "";
+
+        // Does the user have access to view the items?
+        if ($noauth) {
+            $where .= "+access:[* TO $gid]";
+        }
+
+        if ($user->authorize('com_content', 'edit', 'content', 'all')) {
+            $where .= "+state:[0 TO *]";
+        }
+        else {
+            $where .= '+state:[1 TO 1]';
+            $where .= '+(hasPublishUpDate:false OR publish_up:[* TO NOW])';
+            $where .= '+(hasPublishDownDate:false OR publish_down:[NOW TO *])';
+        }
+
+        // Zonal
+        // lista de zonales, zonal actualmente seleccionado
+        require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
+        $helper = new comZonalesHelper();
+        $zonal = $helper->getZonal();
+        $localidades = $helper->getLocalidadesByPartido($zonal->id);
+
+
+        $localidadesList = array();
+        foreach($localidades as $localidad) {
+            $localidadesList[] = $localidad->name;
+        }
+
+        $where .= '+tags_values:('.implode(" ",$localidadesList).')';
+
+        //$where .= $additionalParams;
+
+        return $where;
+    }
+
+    /**
+     *
+     * @return <type>
+     */
+    function getEqPreferences() {
+
+        require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'eq.php');
+        JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'tables');
+        // controladores
+        $ctrlEq = new EqZonalesControllerEq();
+        $ctrlEq->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
+
+        // recupera el usuario
+        $user =& JFactory::getUser();
+
+        $bq = "";
+
+        if (!$user->guest) {
+            // recupera ecualizador del usuario
+            $result = $ctrlEq->retrieveUserEqImpl($user->id);
+
+            if (!is_null($result) && !empty($result)) {
+                $eq = $result[0];
+
+                foreach ($eq->bands as $band) {
+                    $bq .= " tags_values:$band->band_name^$band->peso";
+                }
+            }
+        }
+
+        return $bq;
+
+    }
+
 }
