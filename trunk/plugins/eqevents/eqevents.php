ed43918014e704491c01d0975943d5d44ede1ec4
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-28 13:22:03 +0000

    Agrega ecualizador por defecto a usuarios existentes que no cuenten con uno.

--- trunk/plugins/eqevents/eqevents.php
+++ trunk/plugins/eqevents/eqevents.php
@@ -32,6 +32,17 @@ jimport('joomla.plugin.plugin');
 class plgUserEqevents extends JPlugin {
 
     /**
+     * Controlador Ecualizador
+     * @var EqZonalesControllerEq
+     */
+    var $_ctrlEq = null;
+    /**
+     * Controlador Banda
+     * @var EqZonalesControllerBand
+     */
+    var $_ctrlBand = null;
+
+    /**
      * Constructor
      *
      * For php4 compatability we must not use the __constructor as a
@@ -45,6 +56,65 @@ class plgUserEqevents extends JPlugin {
      */
     function plgUserEqevents(& $subject, $config) {
         parent::__construct($subject, $config);
+
+        /**
+         * Realiza el include de los controladores del componente EqZonales
+         */
+        require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'eq.php');
+        require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'band.php');
+        require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'helper'.DS.'helper.php');
+        JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'tables');
+
+        /**
+         * Crea instancias de los controladores del componente EqZonales,
+         * y les setea el path correcto hacia sus respectivos modelos.
+         */
+        $this->_ctrlEq = new EqZonalesControllerEq();
+        $this->_ctrlEq->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
+        $this->_ctrlBand = new EqZonalesControllerBand();
+        $this->_ctrlBand->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
+    }
+
+    /**
+     * Verifica que un usuario cuente con un Ecualizador, y en caso de no contar
+     * con uno lo genera dinámicamente. Este chequeo puede ser desactivado
+     * desde la configuración del plugin.
+     *
+     * @param Array $user Arreglo con información del usuario.
+     * @param Array $options Arreglo con opciones varias.
+     * @return True En todos los casos. Si falla la creació del Ecualizador el
+     *              modulo correspondiente mostrará un mensaje de error.
+     */
+    function onLoginUser($user, $options) {
+        $create_eq = $this->params->get('create_eq', true);
+
+        // No chequear existencia de un ecualizador
+        if (!$create_eq) {
+            return true;
+        }
+
+        // no se ejecuta si se accede al backend
+        $app = JFactory::getApplication();
+        if ($app->isAdmin()) return true;
+
+        jimport('joomla.user.helper');
+        $instance = new JUser();
+        $id = intval(JUserHelper::getUserId($user['username']));
+        if ($id) {
+            $instance->load($id);
+        }
+
+        $userTmp['id'] = intval($instance->get('id'));
+        $userTmp['name'] = $instance->get('name');
+
+        // Crear un ecualizador si el usuario no cuenta con uno
+        $userEq = $this->_ctrlEq->retrieveUserEqImpl($id);
+
+        if (is_null($userEq) || empty($userEq)) {
+            $this->_createNewDefaultEq($userTmp);
+        }
+
+        return true;
     }
 
     /**
@@ -61,79 +131,41 @@ class plgUserEqevents extends JPlugin {
         global $mainframe;
 
         /**
-         * Si el nuevo usuario ha sido creado en la base de datos...
+         * Si es un nuevo usuario, creamos el ecualizador.
          */
         if ($isnew) {
-            /**
-             * Objeto con información del usurio y ecualizador a crear
-             */
-            $params = new stdClass();
-            $params->user_id = $user['id'];
-            $params->nombre = $user['name'];
-            $params->descripcion = 'Ecualizador de ' . $user['name'];
-            $params->observaciones = 'Ecualizador de ' . $user['name'];
-
-            /**
-             * Realiza el include de los controladores del componente EqZonales
-             */
-            require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'eq.php');
-            require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'band.php');
-            require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'helper'.DS.'helper.php');
-            JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'tables');
-
-            // Instancia el helper de EqZonales
-            $helper = new comEqZonalesHelper();
-
-            /**
-             * Crea instancias de los controladores del componente EqZonales,
-             * y les setea el path correcto hacia sus respectivos modelos.
-             */
-            $ctrlEq = new EqZonalesControllerEq();
-            $ctrlEq->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
-            $ctrlBand = new EqZonalesControllerBand();
-            $ctrlBand->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
-
-            /**
-             * Crea el ecualizador con los datos del nuevo usuario.
-             */
-            $eqId = $ctrlEq->createEq($params);
-            if ($eqId === FALSE) {
-                return;
-            } else {
-                /**
-                 * Instancia las bandas por defecto para el ecualizador.
-                 */
-                $ctrlBand->createDefaultBands($eqId);
-            }
+            $this->_createNewDefaultEq($user);
         }
+
+        return true;
     }
 
     /**
-     * Example store user method
+     * Crea un nuevo ecualizador en base a la información del usuario.
      *
-     * Method is called before user data is deleted from the database
-     *
-     * @param 	array		holds the user data
+     * @param Array $user arreglo con datos del usuario.
      */
-    function onBeforeDeleteUser($user) {
-        global $mainframe;
-    }
+    function _createNewDefaultEq($user) {
+        /**
+         * Objeto con información del usurio y ecualizador a crear
+         */
+        $params = new stdClass();
+        $params->user_id = $user['id'];
+        $params->nombre = $user['name'];
+        $params->descripcion = 'Ecualizador de ' . $user['name'];
+        $params->observaciones = 'Ecualizador de ' . $user['name'];
 
-    /**
-     * Example store user method
-     *
-     * Method is called after user data is deleted from the database
-     *
-     * @param 	array		holds the user data
-     * @param	boolean		true if user was succesfully stored in the database
-     * @param	string		message
-     */
-    function onAfterDeleteUser($user, $succes, $msg) {
-        global $mainframe;
-
-        // only the $user['id'] exists and carries valid information
-
-        // Call a function in the external app to delete the user
-        // ThirdPartyApp::deleteUser($user['id']);
+        /**
+         * Crea el ecualizador con los datos del nuevo usuario.
+         */
+        $eqId = $this->_ctrlEq->createEq($params);
+        if ($eqId === FALSE) {
+            return;
+        } else {
+            /**
+             * Instancia las bandas por defecto para el ecualizador.
+             */
+            $this->_ctrlBand->createDefaultBands($eqId);
+        }
     }
 }
