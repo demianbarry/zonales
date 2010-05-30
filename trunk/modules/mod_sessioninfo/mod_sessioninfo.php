f2a2461ee09016e7be8f8a06a45128222e4cbd83
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-30 23:32:06 +0000

    Se agregaron controles de seguridad de Joomla. El código JS fue trasladado al template.

--- trunk/modules/mod_sessioninfo/mod_sessioninfo.php
+++ trunk/modules/mod_sessioninfo/mod_sessioninfo.php
@@ -1,17 +1,27 @@
 <?php
+/**
+ * @version	$Id$
+ * @package	Zonales
+ * @copyright	Copyright (C) 2009 Mediabit. All rights reserved.
+ * @license	GNU/GPL, see LICENSE.php
+ *
+ * Zonales is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
 $user =& JFactory::getUser();
 $session =& JFactory::getSession();
 
-    $profileLink = $params->get('profilelink');
-    $greetingMessage = sprintf(JText::_('ZONALES_SESSION_GREETING'),$user->get('name'));
-    $sessionCloseMessage = JText::_('ZONALES_SESSION_CLOSE');
-    $logoutRoute = JRoute::_('index.php?option=com_user&task=logout');
-    $protocol = $session->get('accessprotocol');
-    require(JModuleHelper::getLayoutPath('mod_sessioninfo'));
- ?>
-<script text="text/javascript">
-    function logout(){
-        <?php echo ($protocol == 'Facebook Connect') ? 'FB.Connect.logout(function() {  });' : '' ?>
-        window.location.href='<?php echo $logoutRoute ?>';
-    }
-</script>
+$profileLink = $params->get('profilelink');
+$greetingMessage = sprintf(JText::_('ZONALES_SESSION_GREETING'),$user->get('name'));
+$sessionCloseMessage = JText::_('ZONALES_SESSION_CLOSE');
+$logoutRoute = JRoute::_('index.php?option=com_user&task=logout');
+$protocol = $session->get('accessprotocol');
+
+require(JModuleHelper::getLayoutPath('mod_sessioninfo'));
