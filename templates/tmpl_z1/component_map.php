fcf2938d00970124ba547b9d6827620759657032
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-05-31 20:19:02 +0000

    Se modifico el template para que siempre mostrara maximizada las ventanas

--- templates/tmpl_z1/component_map.php
+++ templates/tmpl_z1/component_map.php
@@ -1,7 +1,13 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" xmlns:fb="http://www.facebook.com/2008/fbml" >
-<head>
+<script language="JavaScript">
+
+window.moveTo(0,0);
+window.resizeTo(screen.width,screen.height);
+
+</script>
+    <head>
 <jdoc:include type="head" />
 
 <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/reset.css" />
