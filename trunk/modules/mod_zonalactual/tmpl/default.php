0959e6e35eb8ee4ce692dc614ecf2562dae496ac
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-01 19:02:34 +0000

    Modifiqué Zonal Actual para que muestre todas las Zonas

--- trunk/modules/mod_zonalactual/tmpl/default.php
+++ trunk/modules/mod_zonalactual/tmpl/default.php
@@ -23,7 +23,7 @@ defined( '_JEXEC' ) or die ( 'Restricted Access' );
 	<img alt="<?php echo $zonalName; ?>" class="zonalactual" title="<?php echo $zonalName; ?>"
 	     src="templates/<?php echo $template; ?>/images/<?php echo $mainColor; ?>/logozonal.gif" />
         <div style="position: relative; top: -43px; left: 12px;">
-            <p style="color: white; font-family: fantasy; font: bold; font-size: 12px; text-transform: uppercase;">
+            <p style="color: white; font-family: fantasy; font: bold; font-size: 12px; text-transform: uppercase; margin-right: 13px;">
                 <?php echo $zonalName; ?>
             </p>
         </div>
