a270e3234e902dfe417c696342ba6e33e37719bc
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-03 20:10:15 +0000

    Ecualizador para usuario anónimo.

--- trunk/components/com_eqzonales/admin/controller.php
+++ trunk/components/com_eqzonales/admin/controller.php
@@ -10,49 +10,101 @@ jimport('joomla.filesystem.file');
  * Controlador
  *
  */
-class EqZonalesController extends JController
-{
-	/**
-	 * Constructor
-	 *
-	 * @param array $default
-	 */
-	function __construct($default = array())
-	{
-		parent::__construct($default);
-	}
+class EqZonalesController extends JController {
+    /**
+     * Constructor
+     *
+     * @param array $default
+     */
+    function __construct($default = array()) {
+        parent::__construct($default);
+    }
 
-        function listEq()
-	{
-		$this->baseDisplayTask('ListaEq', '');
-	}
+    function listaEq() {
+        $this->baseDisplayTask('ListaEq', '');
+    }
 
-        function baseDisplayTask($view, $modelName, $layout = 'default', $hidemainmenu = 0, $vars = array())
-	{
-		global $option;
+    function addAnonDefaultEq() {
+        global $option;
 
-		$document = &JFactory::getDocument();
-		$vType = $document->getType();
-		$vName = JRequest::getCmd('view', $view);
-		$vLayout = JRequest::getCmd( 'layout', $layout );
+        $params = &JComponentHelper::getParams( 'com_eqzonales' );
+        $eq_anon_id = $params->get( 'eq_anon_id', null );
+        $eq_anon_name = $params->get( 'eq_anon_name', null );
 
-		// Get/Create the view
-		$view = &$this->getView( $vName, $vType);
+        if (is_null($eq_anon_id)) {
+            echo "<script> alert('No se especifico un id para el Ecualizador Anónimo'); window.history.go(-1); </script>\n";
+            exit();
+        }
 
-		// Get/Create the models
-		$model = &$this->getModel($modelName);
-		if ($model)
-		{
-			$view->setModel($model, true);
-		}
+        /**
+         * Realiza el include de los controladores del componente EqZonales
+         */
+        require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'eq.php');
+        require_once(JPATH_BASE.DS.'components'.DS.'com_eqzonales'.DS.'controllers'.DS.'band.php');
+        require_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'helper'.DS.'helper.php');
+        JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'tables');
 
-		// Desactiva el menú principal
-		JRequest::setVar('hidemainmenu', $hidemainmenu);
+        /**
+         * Crea instancias de los controladores del componente EqZonales,
+         * y les setea el path correcto hacia sus respectivos modelos.
+         */
+        $ctrlEq = new EqZonalesControllerEq();
+        $ctrlEq->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
+        $ctrlBand = new EqZonalesControllerBand();
+        $ctrlBand->addModelPath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eqzonales'.DS.'models' );
 
-		// Set the layout
-		$view->setLayout($vLayout);
+        $params = new stdClass();
+        $params->id = $eq_anon_id;
+        $params->user_id = 0;
+        $params->nombre = $eq_anon_name;
+        $params->descripcion = $eq_anon_name;
+        $params->observaciones = $eq_anon_name;
 
-		// Display the view
-		$view->display();
-	}
+        /**
+         * Crea el ecualizador con los datos del nuevo usuario.
+         */
+        $eqId = $ctrlEq->createEq($params);
+        if ($eqId === FALSE) {
+            echo "<script> alert('Error al crear el Ecualizador Anónimo'); window.history.go(-1); </script>\n";
+            exit();
+        } else {
+            $params->id = $eqId;
+            /**
+             * Instancia las bandas por defecto para el ecualizador.
+             */
+            $ctrlBand->createDefaultBands($params);
+        }
+
+        $link = 'index.php?option=' . $option;
+        $msg = 'Ecualizador Anónimo creado exitosamente';
+
+        $this->setRedirect($link, $msg);
+    }
+
+    function baseDisplayTask($view, $modelName, $layout = 'default', $hidemainmenu = 0, $vars = array()) {
+        global $option;
+
+        $document = &JFactory::getDocument();
+        $vType = $document->getType();
+        $vName = JRequest::getCmd('view', $view);
+        $vLayout = JRequest::getCmd( 'layout', $layout );
+
+        // Get/Create the view
+        $view = &$this->getView( $vName, $vType);
+
+        // Get/Create the models
+        $model = &$this->getModel($modelName);
+        if ($model) {
+            $view->setModel($model, true);
+        }
+
+        // Desactiva el menú principal
+        JRequest::setVar('hidemainmenu', $hidemainmenu);
+
+        // Set the layout
+        $view->setLayout($vLayout);
+
+        // Display the view
+        $view->display();
+    }
 }
