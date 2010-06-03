efde701e5d03a0a19622dc9bcb0ea6bc86ed119c
laykondash <laykondash@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-03 22:54:58 +0000

    Se modifico que el template para que siempre mostrara maximizado el sitio

--- trunk/templates/tmpl_z1/component_map.php
+++ trunk/templates/tmpl_z1/component_map.php
@@ -1,12 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" xmlns:fb="http://www.facebook.com/2008/fbml" >
-<script language="JavaScript">
-
-window.moveTo(0,0);
-window.resizeTo(screen.width * 0.99,screen.height * 0.99);
-
-</script>
     <head>
 <jdoc:include type="head" />
 
@@ -15,6 +9,7 @@ window.resizeTo(screen.width * 0.99,screen.height * 0.99);
 <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/content.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/modules.css" />
 
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/window.js"></script>
 <?php JHTML::script('swfobject.js'); ?>
 </head>
 
