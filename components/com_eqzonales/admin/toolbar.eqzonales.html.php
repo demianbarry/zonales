150b46e978378df0b9aa34eae37deb0049750528
franpaez <franpaez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-14 21:00:31 +0000

    Elimina (filtra) de los contenidos recuperados aquellos que tengan un peso igual a cero en el ecualizador.

--- components/com_eqzonales/admin/toolbar.eqzonales.html.php
+++ components/com_eqzonales/admin/toolbar.eqzonales.html.php
@@ -7,7 +7,6 @@ class TOOLBAR_eqzonales_default
 {
 	function _DEFAULT() {
 		JToolBarHelper::title( JText::_( 'EQZONALES'));
-                //JToolBarHelper::addNewX('addAnonDefaultEq', 'Crear Ecualizador Anónimo');
                 JToolBarHelper::custom( 'addAnonDefaultEq', 'download.png', 'download.png', 'Crear Ecualizador Anónimo', false, false);
 	        JToolBarHelper::preferences('com_eqzonales', 400, 575);
 	}
