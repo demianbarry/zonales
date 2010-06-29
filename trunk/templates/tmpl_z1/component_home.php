ea371ced6758084d5e5987c11bffeeee4f243525
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-29 23:57:05 +0000

    Template con nuevo Home y arreglos en comboZona

---
+++ trunk/templates/tmpl_z1/component_home.php
@@ -0,0 +1,55 @@
+<?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" xmlns:fb="http://www.facebook.com/2008/fbml" >
+    <head>
+        <jdoc:include type="head" />
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/content.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/modules.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/home.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/css/_<?php echo $this->params->get('mainColor'); ?>.css" />
+
+        <script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/js/window.js"></script>
+        <?php JHTML::script('swfobject.js'); ?>
+    </head>
+
+    <body>
+        <center>
+            <div id="home_top">
+            </div>
+
+            <div id="home_zona">
+                <div id="home_zona_text">
+                    Aca va texto, efectos, imágenes, etc. referidos a la zona
+                </div>
+                <div id="home_zona_mod">
+                    <jdoc:include type="modules" name="home_zona" style="xhtml" />
+                </div>
+
+            </div>
+
+            <div id="home_login">
+                <div id="home_login_text">
+                    Sumate a zonales!!. Textos, imágenes, efectos referidos invitando a los usuarios a Zonales
+                </div>
+                <div id="home_login_mod">
+                    <jdoc:include type="modules" name="home_login" style="xhtml" />
+                </div>
+            </div>
+
+            <div id="copy">
+                Copyright 2009 ZONALES.COM.AR | Todos los derechos reservados. <a href="http://validator.w3.org/check?uri=referer"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $mainframe->getTemplate(); ?>/images/w3c_xhtml_01.gif" alt="Valid XHTML 1.0 Transitional" /></a>
+            </div><!-- END #copy -->
+
+            <?php
+            $user =& JFactory::getUser();
+            if ($user->guest):
+                ?>
+            <script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/es_LA"></script>
+            <script type="text/javascript">  FB.init("91778705a13235cd3efe59d31e4d31bf","index.php?option=com_user&task=login&provider=Facebook/xd_receiver.htm");</script>
+            <?php endif ?>
+        </center>
+    </body>
+</html>
--- trunk/templates/tmpl_z1/index.php
+++ trunk/templates/tmpl_z1/index.php
@@ -2,12 +2,17 @@
 require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 $helper = new comZonalesHelper();
 
-$view = 'component.php';
+//$view = 'component.php'; Comentado por Nacho
 
 if(is_null($helper->getZonal()) && !$helper->isAuthenticationOnProgress() && $helper->showMap())
 {
 // si no se ha seleccionado un zonal o no hay ninguna autenticacion en progreso
-$view = 'component_map.php';
+//$view = 'component_map.php';
+    $view = 'component_home.php';
+}
+/************** Agregado Nacho ****************/
+else {
+    $view = 'component.php';
 }
 include dirname(__FILE__) . DIRECTORY_SEPARATOR . $view;
 ?>
