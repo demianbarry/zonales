10999f9ed73c7ea6a9dac4f320b1bbe13796dc55
juanmanuelcortez <juanmanuelcortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-30 14:41:39 +0000

    Cambios menores de estilo

--- trunk/modules/mod_zcloud/tmpl/default.php
+++ trunk/modules/mod_zcloud/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
 
-<div class="zcloud_container" style="position: relative; overflow: hidden; width: <?php echo $width; ?>px;">
+<div class="zcloud_container">
 	<div class="jcloud_tags" style="text-align: center; line-height: 1.6em;">
 		<?php foreach( $relevances as $tag => $count ):
                 $size = ( ( $maxsize * 0.65 ) * ( $count - $min ) / $max ) + ( $maxsize * 0.35 );
