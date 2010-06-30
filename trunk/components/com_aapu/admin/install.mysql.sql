7bbf516b67208bb69dfc871b4d45b208646ea854
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-06-30 14:55:19 +0000

    Corrección zona del usuario

--- trunk/components/com_aapu/admin/install.mysql.sql
+++ trunk/components/com_aapu/admin/install.mysql.sql
@@ -72,5 +72,5 @@ INSERT INTO `#__aapu_data_types` (`id`, `label`, `description`, `render`) VALUES
 INSERT INTO `#__aapu_attributes` (`id`, `name`, `label`, `description`, `comments`, `from`, `to`, `canceled`, `required`, `published`, `validator`, `data_type_id`, `attribute_class_id`, `values_list`) VALUES
 (1, 'sex', 'Sexo', 'Sexo del usuario', '', '2010-04-08', '0000-00-00', 0, 1, 1, 'validator_for_SEX_attributes.php', 6, 1, null),
 (2, 'birthday', 'Fecha de Nacimiento', 'Fecha de Nacimiento', '', '2010-04-09', '0000-00-00', 0, 1, 1, 'validator_for_DATES_attributes.php', 4, 1, null),
-(3, 'zonal', 'Zonal de Preferencia', 'Zonal de Preferencia del usuario', '', '2010-05-04', '0000-00-00', 0, 1, 1, '', 8, 1, 'SELECT v.id, v.label FROM jos_custom_properties_values v WHERE v.field_id = (SELECT f.id FROM jos_custom_properties_fields f WHERE f.name = "root_zonales") AND v.name LIKE "bue\\_%";'),
+(3, 'zonal', 'Zonal de Preferencia', 'Zonal de Preferencia del usuario', '', '2010-05-04', '0000-00-00', 0, 1, 1, '', 8, 1, 'SELECT v.id, v.label FROM jos_custom_properties_values v WHERE v.field_id = (SELECT f.id FROM jos_custom_properties_fields f WHERE f.name = "root_zonales");'),
 (4, 'email2', 'E-Mail de respaldo', 'E-Mail de respaldo', '', '2010-05-04', '0000-00-00', 0, 0, 1, 'validator_for_MAILS_attributes.php', 1, 1, '');
