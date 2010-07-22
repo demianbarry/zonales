95cff8f8b17684d91b5290abf80f90088caf3097
laykondash@gmail.com <laykondash@gmail.com@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-07-22 20:23:32 +0000

    Se modifico la vista de solicitud de situacion para que habilitara un mensaje en la proxima pantalla

--- components/com_user/views/userstatusrequest/view.html.php
+++ components/com_user/views/userstatusrequest/view.html.php
@@ -3,6 +3,7 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.application.component.view');
+require_once JPATH_ROOT . DS . 'modules' . DS . 'mod_message' . DS . 'constants.php';
 
 class UserViewUserStatusRequest extends JView {
 
@@ -15,7 +16,7 @@ class UserViewUserStatusRequest extends JView {
         $iamuserMessage = JText::_('ZONALES_STATUS_USER');
         $notuserMessage = JText::_('ZONALES_STATUS_GUEST');
 
-        $urlLogin = 'index.php?option=com_user&view=zlogin&map=0&externalid='.urlencode($externalid). '&providerid=' . $providerid.'&' . JUtility::getToken() .'=1';
+        $urlLogin = 'index.php?option=com_user&view=zlogin&map=0&externalid='.urlencode($externalid). '&providerid=' . $providerid.'&' . JUtility::getToken() .'=1&status='.INFO . '&message='.urlencode(JText::_('SYSTEM_USER_ALIAS_NOT_FOUND_AUTHENTICATE'));
         $urlRegister = 'index.php?option=com_user&view=register&force=1&map=0&externalid='.urlencode($externalid).'&providerid='.$providerid.'&' . JUtility::getToken() .'=1';
 
         $this->assignRef('urlLogin', $urlLogin);
