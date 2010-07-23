7e689fceaab40f8ff7373d2a2055cbf88b55a7cf
juanmanuelcortez <juanmanuelcortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-07-23 20:55:40 +0000

    ligero cambio de estilo

--- trunk/modules/mod_message/tmpl/default.php
+++ trunk/modules/mod_message/tmpl/default.php
@@ -1,5 +1,7 @@
 &nbsp;
-<p class="<?php echo $class ?>">
-    <img class="image" src="<?php echo $icon ?>" alt="<?php echo $messageType ?>"/>
-    <?php echo $message ?>
-</p>
+<div style="color:#666666; font-family:'trebuchet MS';font-size:14px;margin-bottom:20px;">
+    <p class="<?php echo $class ?>">
+        <img class="image" src="<?php echo $icon ?>" alt="<?php echo $messageType ?>"/>
+        <?php echo $message ?>
+    </p>
+</div>
