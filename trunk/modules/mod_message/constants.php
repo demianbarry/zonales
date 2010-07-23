64bf25a17a786aafd1364e8f411400677e878e54
laykondash@gmail.com <laykondash@gmail.com@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-07-23 20:49:28 +0000

    Se modifico el modulo mod_message para que tomara los parametros de la sesion

--- trunk/modules/mod_message/constants.php
+++ trunk/modules/mod_message/constants.php
@@ -5,5 +5,7 @@ if (!defined('ERROR')) define('ERROR',1,true);
 if (!defined('WARNING')) define('WARNING',2,true);
 if (!defined('INFO')) define('INFO',3,true);
 if (!defined('ACTION')) define('ACTION',4,true);
+if (!defined('MMMESSAGE')) define('MMMESSAGE','mmmessage',true);
+if (!defined('MMSTATUS')) define('MMSTATUS','mmstatus',true);
 
 ?>
