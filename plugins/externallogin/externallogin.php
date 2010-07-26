3bffcd85b93a1a31cf9111d5dcc1749cb11b07d7
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-07-26 19:42:01 +0000

    Corrección External Login

--- plugins/externallogin/externallogin.php
+++ plugins/externallogin/externallogin.php
@@ -22,7 +22,13 @@ jimport('joomla.user.helper');
 jimport('joomla.utilities.utility');
 
 require_once 'externalauth.php';
-require_once JPATH_ROOT . DS . 'modules' . DS . 'mod_message' . DS . 'constants.php';
+if (!defined('SUCCESS')) define('SUCCESS',0,true);
+if (!defined('ERROR')) define('ERROR',1,true);
+if (!defined('WARNING')) define('WARNING',2,true);
+if (!defined('INFO')) define('INFO',3,true);
+if (!defined('ACTION')) define('ACTION',4,true);
+if (!defined('MMMESSAGE')) define('MMMESSAGE','mmmessage',true);
+if (!defined('MMSTATUS')) define('MMSTATUS','mmstatus',true);
 
 /**
  * OpenID Authentication Plugin
