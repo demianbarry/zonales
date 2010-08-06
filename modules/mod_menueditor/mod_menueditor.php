cb21ed2e97179b9ec9f3d41d6ea81dc2a4452db8
juanmanuelcortez <juanmanuelcortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-08-06 14:24:15 +0000

    Cambios en url de denunciados.

--- modules/mod_menueditor/mod_menueditor.php
+++ modules/mod_menueditor/mod_menueditor.php
@@ -21,6 +21,6 @@ $createArticleRoute = JRoute::_('index.php?view=article&task=edit&option=com_con
 $viewPublishedArticles = JRoute::_('index.php?view=myarchive&stateFrom=1&stateTo=1&option=com_content&tmpl=component_edit', false);
 $viewUnpublishedArticles = JRoute::_('index.php?view=myarchive&stateFrom=0&stateTo=0&option=com_content&tmpl=component_edit', false);
 $viewUnpublishedArticles = JRoute::_('index.php?view=myarchive&stateFrom=0&stateTo=0&option=com_content&tmpl=component_edit', false);
-$viewDenouncedArticles = JRoute::_('index.php?view=myarchive&stateFrom=5&stateTo=5&option=com_content&tmpl=component_edit', false);
+$viewDenouncedArticles = JRoute::_('index.php?view=myarchive&getDenounced=1&option=com_content&tmpl=component_edit', false);
 
 require(JModuleHelper::getLayoutPath('mod_menueditor'));
