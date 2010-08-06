174e67c5b6bd99241c3b2b1efb1253d4c4495288
juanmanuelcortez <juanmanuelcortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2010-08-06 14:15:31 +0000

    Cambios de estilo en módulo sessioninfo. Error en SQL de auth_util.php.

--- trunk/templates/tmpl_z1/auth_util.php
+++ trunk/templates/tmpl_z1/auth_util.php
@@ -1,7 +1,7 @@
 <?php
 
 function getApiKey($provider){
-    $query = 'select p.apikey from #__providers where name="' . $provider . '"';
+    $query = 'select p.apikey from #__providers p where name="' . $provider . '"';
     $db = JFactory::getDBO();
     $db->setQuery($query);
     $dbApi = $db->loadObject();
