68bf5a6a8e435b6b8c9b0ac68bd9ba81d82c3f63
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2011-04-11 12:43:18 +0000

    Limité el login solo a Zonales. Se irán agregando otros métodos a medida que se vayan corrigiendo.

--- modules/mod_userregister/mod_userregister.php
+++ modules/mod_userregister/mod_userregister.php
@@ -67,7 +67,7 @@ require_once JPATH_ROOT . DS . 'components' . DS . 'com_user' . DS . 'helper.php
         $db =& JFactory::getDBO();
         $selectProviders = 'select p.name, p.icon_url, g.name as groupname, p.required_input, t.name as type ' .
             'from #__providers p, #__groups g, #__protocol_types t ' .
-            'where p.access=g.id and t.id=p.protocol_type_id ' .
+            'where p.access=g.id and p.enabled=1 and t.id=p.protocol_type_id ' .
             'order by (select count(*) from #__alias a where p.id=a.provider_id) desc, t.name  asc';
 
         $db->setQuery($selectProviders);
