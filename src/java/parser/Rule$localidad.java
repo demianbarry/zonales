a81ce82bc2d4e5a6e4aaa8c1d071fdab1b17f286
juanmanuelcortez <juanmanuelcortez@7b6a804e-3e55-11de-9d05-31133e6853b1>
2011-08-05 14:42:28 +0000

    Última versión ZCrawl Parser Servlet

---
+++ src/java/parser/Rule$localidad.java
@@ -0,0 +1,80 @@
+/* -----------------------------------------------------------------------------
+ * Rule$localidad.java
+ * -----------------------------------------------------------------------------
+ *
+ * Producer : com.parse2.aparse.Parser 2.0
+ * Produced : Thu Aug 04 12:08:11 ART 2011
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+package parser;
+
+import java.util.ArrayList;
+
+final public class Rule$localidad extends Rule
+{
+  private Rule$localidad(String spelling, ArrayList<Rule> rules)
+  {
+    super(spelling, rules);
+  }
+
+  public Object accept(Visitor visitor)
+  {
+    return visitor.visit(this);
+  }
+
+  public static Rule$localidad parse(ParserContext context)
+  {
+    context.push("localidad");
+
+    boolean parsed = true;
+    int s0 = context.index;
+    ArrayList<Rule> e0 = new ArrayList<Rule>();
+    Rule rule;
+
+    parsed = false;
+    if (!parsed)
+    {
+      {
+        ArrayList<Rule> e1 = new ArrayList<Rule>();
+        int s1 = context.index;
+        parsed = true;
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule$cadena.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+          e0.addAll(e1);
+        else
+          context.index = s1;
+      }
+    }
+
+    rule = null;
+    if (parsed)
+      rule = new Rule$localidad(context.text.substring(s0, context.index), e0);
+    else
+      context.index = s0;
+
+    context.pop("localidad", parsed);
+
+    return (Rule$localidad)rule;
+  }
+}
+
+/* -----------------------------------------------------------------------------
+ * eof
+ * -----------------------------------------------------------------------------
+ */
--- test/com_eqzonales/TestBand.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class TestBand extends UnitTestCase {
-
-    
-
-}
