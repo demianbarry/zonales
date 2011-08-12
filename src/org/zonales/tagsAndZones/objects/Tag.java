497c8bfb2367de78636f25f9b65c23499cce91ec
rodrigojaramillo@gmail.com <rodrigojaramillo@gmail.com@7b6a804e-3e55-11de-9d05-31133e6853b1>
2011-08-12 15:39:23 +0000

    Tags and Zones - Retrieve All

--- src/org/zonales/tagsAndZones/objects/Tag.java
+++ src/org/zonales/tagsAndZones/objects/Tag.java
@@ -23,6 +23,11 @@ public class Tag {
         this.name = name;
     }
 
+    public Tag(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Tag(int id, String name, Tag parent, Type type, String state) {
         this.id = id;
         this.name = name;
