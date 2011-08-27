c69e466f6f143b387a603600ab7ba9cbc99db9b2
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2011-08-27 15:06:46 +0000

    Cralws Schedule extractions

--- modules/mod_message/tmpl/default.php
+++
@@ -1,7 +0,0 @@
-&nbsp;
-<div style="color:#666666; font-family:'trebuchet MS';font-size:14px;margin-bottom:20px;">
-    <p class="<?php echo $class ?>">
-        <img class="image" src="<?php echo $icon ?>" alt="<?php echo $messageType ?>"/>
-        <?php echo $message ?>
-    </p>
-</div>
