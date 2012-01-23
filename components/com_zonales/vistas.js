a58c11aa32b0deff7e4c0266c2021aca86dc2c49
rodrigojaramillo@gmail.com <rodrigojaramillo@gmail.com@7b6a804e-3e55-11de-9d05-31133e6853b1>
2012-01-23 12:54:24 +0000

    Refactoring com_zonales

--- components/com_zonales/vistas.js
+++ components/com_zonales/vistas.js
@@ -248,7 +248,7 @@ function searchPost(keyword, zone) {
             } else {
                 if (zcGetContext().efZone != '') {
                     new Element('label', {
-                        'html': 'No se encontraron resultados para su bÃºsqueda en la zona seleccionada'
+                        'html': 'No se encontraron resultados para su busqueda en la zona seleccionada'
                     }).inject($('postsContainer'));
                     new Element('input', {
                         'type': 'button', 
@@ -257,7 +257,7 @@ function searchPost(keyword, zone) {
                     }).inject($('postsContainer'));
                 } else {
                     new Element('label', {
-                        'html': 'No se encontraron resultados para su bÃºsqueda'
+                        'html': 'No se encontraron resultados para su busqueda'
                     }).inject($('postsContainer'));
                 }
             }
@@ -818,13 +818,13 @@ function prettyDate(time){
     [7200, ' hace 1 hora', 'hace 1 hora'], // 60*60*2
     [86400, 'horas', 3600], // 60*60*24, 60*60
     [172800, '1 dia', 'maÃ±ana'], // 60*60*24*2
-    [604800, 'dÃ­as', 86400], // 60*60*24*7, 60*60*24
-    [1209600, ' en la ultima semana', 'prÃ³xima semana'], // 60*60*24*7*4*2
+    [604800, 'dias', 86400], // 60*60*24*7, 60*60*24
+    [1209600, ' en la ultima semana', 'proxima semana'], // 60*60*24*7*4*2
     [2419200, 'semanas', 604800], // 60*60*24*7*4, 60*60*24*7
-    [4838400, 'ultimo mes', 'prÃ³ximo mes'], // 60*60*24*7*4*2
+    [4838400, ' ultimo mes', 'proximo mes'], // 60*60*24*7*4*2
     [29030400, 'meses', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-    [58060800, ' en el ultimo aÃ±o', 'proximo aÃ±o'], // 60*60*24*7*4*12*2
-    [2903040000, 'aÃ±os', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+    [58060800, ' en el ultimo año', 'proximo año'], // 60*60*24*7*4*12*2
+    [2903040000, 'años', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
     [5806080000, 'ultimo siglo', 'proximo siglo'], // 60*60*24*7*4*12*100*2
     [58060800000, 'siglos', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
     ];
