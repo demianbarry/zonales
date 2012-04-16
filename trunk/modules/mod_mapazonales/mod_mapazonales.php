1efca33ba806a703bec950e21b6772159134af7a
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2012-04-16 13:37:32 +0000

    Nuevo Template Z30

--- trunk/modules/mod_mapazonales/mod_mapazonales.php
+++ trunk/modules/mod_mapazonales/mod_mapazonales.php
@@ -6,22 +6,22 @@ require_once (JPATH_BASE.DS.'components'.DS.'com_zonales'.DS.'helper.php');
 jimport ('joomla.filesystem.file');
 
 // parametros
-$showLabel = $params->get('show_label', 'show_label');
-$labelText = $params->get('label_text', 'label_text');
-$titleText = $params->get('title_text', 'title_text');
-$height = $params->get('height_popup');
-$width = $params->get('width_popup');
-$useFlash = $params->get('use_flash', false);
+//$showLabel = $params->get('show_label', 'show_label');
+//$labelText = $params->get('label_text', 'label_text');
+//$titleText = $params->get('title_text', 'title_text');
+//$height = $params->get('height_popup');
+//$width = $params->get('width_popup');
+//$useFlash = $params->get('use_flash', false);
 
 // template
 $app =& JFactory::getApplication();
 $template = $app->getTemplate();
 
-// valores para los parametros del template
-$tparams = new JParameter(JFile::read(JPATH_BASE.DS.'templates'.DS.
-		$template.DS.'params.ini'));
-
-$mainColor = $tparams->get('mainColor', null);
-if ($mainColor) $mainColor = '_' . $mainColor;
+//// valores para los parametros del template
+//$tparams = new JParameter(JFile::read(JPATH_BASE.DS.'templates'.DS.
+//		$template.DS.'params.ini'));
+//
+//$mainColor = $tparams->get('mainColor', null);
+//if ($mainColor) $mainColor = '_' . $mainColor;
 
 require(JModuleHelper::getLayoutPath('mod_mapazonales'));
--- trunk/modules/mod_mapazonales/tmpl/default.php
+++ trunk/modules/mod_mapazonales/tmpl/default.php
@@ -1,44 +1,13 @@
-<?php
-// Check to ensure this file is included in Joomla!
-defined( '_JEXEC' ) or die ( 'Restricted Access' );
-?>
+<script language="javascript" type="text/javascript" src="modules/mod_mapazonales/map2.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="/media/system/js/OpenLayers.js"></script>
 
-<script type="text/javascript" defer="defer">
-    // <![CDATA[
-    window.addEvent('domready', function() {
-        Shadowbox.init({
-            skipSetup: true,
-            text: {
-                cancel:     'Cancelar',
-                loading:    'Cargando...',
-                close:      '<span class="shortcut">C</span>errar',
-                next:       '<span class="shortcut">S</span>iguiente',
-                prev:       '<span class="shortcut">A</span>nterior',
-                errors:     {
-                    single: 'Usted debe instalar el plugin <a href="{0}">{1}</a> para poder ver el contenido.',
-                    shared: 'Usted debe instalar <a href="{0}">{1}</a> y <a href="{2}">{3}</a> para poder visualizar el contenido.',
-                    either: 'Usted debe instalar <a href="{0}">{1}</a> o <a href="{2}">{3}</a> para poder visualizar el contenido.'
-                }
-            }
-        });
-
-        Shadowbox.setup($('zonal'), {
-            onClose:function() { window.location.reload(true); }
-        });
-    });
-
-    // ]]>
-</script>
-
-<div id="moduletable_zMap">
-    <?php if($showLabel): ?>
-    <p>
-            <?php echo $labelText; ?>
-    </p>
-    <?php endif; ?>
-    <p>
-        <a id="zonal" rel="lightbox;width=<?php echo $width; ?>;height=<?php echo $height; ?>" title="<?php echo $titleText; ?>" href="index.php?option=com_zonales&view=mapa&ajax=true&tmpl=component_only">
-            <img src="templates/<?php echo $template; ?>/images/<?php echo $mainColor; ?>/bot_zonales.gif" />
-        </a>
-    </p>
+<div id="mapDiv">
+    <div id="breadCrumbDiv">
+        <p>
+            <label>Ud. está cerca de: </label>
+            <spam id="breadCrumb"></spam>
+        </p>
+    </div>
+    <div id='map_element' style="width: 96%; height: 530px; border: solid 9px #F1F2F3;"></div>
 </div>
