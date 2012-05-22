a828169b7672ef42c2520896e3eaa2d437ffc1d3
ignacioaita <ignacioaita@7b6a804e-3e55-11de-9d05-31133e6853b1>
2012-05-22 11:53:08 +0000

    Comentarios

--- branches/ZCrawlFeedsServlet/src/java/org/zonales/test2.java
+++ branches/ZCrawlFeedsServlet/src/java/org/zonales/test2.java
@@ -2,8 +2,8 @@ package org.zonales;
 
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
  */
 /**
  *
@@ -30,48 +30,38 @@ import java.util.regex.Pattern;
 public class test2 {
 
     public static void main(String[] args) throws IOException {
-        /*  String url = "http://www.tiemposur.com.ar/nota/38716-unas-mil-personas-se-concentraron-en-repudio-a-las-pol%C3%ADticas-del-gobierno";//args[0];
-        //file:///home/emmanuel/Escritorio/prueba.html
-        print("Fetching %s...", url);
-        //div#contenedorComentariosRecientes
-        Pattern patron;
-        //html.js body form#f1 div#container div.main div#colizq section.news_detail article div#modComments.module-contents section.section_comments div.comentarios div#comments_contents div#comments_listado div#comment_310544.comments_list div.comment_msg div#comment_310544_text h4
-        Document doc = Jsoup.connect(url).get();
-        //html.js body form#f1 div#container div.main div#colizq section.news_detail article div#modComments.module-contents section.section_comments div.comentarios div#comments_contents div#comments_listado div#comment_310483.comments_list div.comment_top span.XXnot_com a
-        Elements posteos = doc.select("div.container_comentarios_row"); //obtengo los post.
-        Elements author;
-        patron = Pattern.compile("( Votar:)");
-        Matcher matcher;
-        for (Element posteo : posteos) {
+//        String url = "http://www.sur54.com/recorte-a-municipios-el-gobierno-avanza-sobre-el-superior-tribunal-de-justicia";//args[0];
+//        //file:///home/emmanuel/Escritorio/prueba.html
+//        print("Fetching %s...", url);
+//        //div#contenedorComentariosRecientes
+//        Pattern patron;
+//        //html.js body form#f1 div#container div.main div#colizq section.news_detail article div#modComments.module-contents section.section_comments div.comentarios div#comments_contents div#comments_listado div#comment_310544.comments_list div.comment_msg div#comment_310544_text h4
+//        Document doc = Jsoup.connect(url).get();
+//        //html.js body form#f1 div#container div.main div#colizq section.news_detail article div#modComments.module-contents section.section_comments div.comentarios div#comments_contents div#comments_listado div#comment_310483.comments_list div.comment_top span.XXnot_com a
+//        Elements posteos = doc.select("ul.comment li p.comment"); //obtengo los post.
+//        Elements author;
+//        patron = Pattern.compile("^[^<].*");
+//        Matcher matcher;
+//        for (Element posteo : posteos) {
+////            author = posteo.select("span.XXnot_com");
+//            matcher = patron.matcher(posteo.html());
+//            if (matcher.find()) {
+//                System.out.println("Matchea: " + posteo.html());
+//            } else {
+//                System.out.println("No matchea: " + posteo.html());
+//            }
+//            //System.out.println(matcher.group(1));
+//            
+//        }
 
-        author = posteo.select("span.XXnot_com");
-        matcher = patron.matcher(author.text());
-        matcher.find();
-        //System.out.println(matcher.group(1));
-        System.out.println(author.text().replaceAll(matcher.group(1), ""));
-        }*/
-        // CrawlSpecification crawler = new CrawlSpecification("http://canchallena.lanacion.com.ar/1473399-la-pelea-de-caruso-lombardi-exploto-en-twitter");
-        //   System.out.println(crawler.toString());
-        /*
-        patron = Pattern.compile("( Votar:)");
-        Matcher matcher;
-        for (Element posteo : posteos) {
-
-        author  = posteo.select("span.XXnot_com");
-        matcher = patron.matcher(author.text());
-        matcher.find();
-        //System.out.println(matcher.group(1));
-        System.out.println(author.text().replaceAll(matcher.group(1), ""));
-        }
-        JUE 19 ABR 2012
-         */
-        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("EEE dd MMM yyyy");
-        String strFecha = "JUE 19 ABR 2012";
+         
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("- dd/mm/aaaa HH:mm:ss");
+        String strFecha = "- 21/05/2012 00:47:12";
         Date fecha = null;
         try {
 
             fecha = formatoDelTexto.parse(strFecha);
-             System.out.println(fecha.toString());
+             System.out.println(fecha.getTime());
         } catch (ParseException ex) {
             ex.printStackTrace();
         }
